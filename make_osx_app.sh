c647de84193cd5892745e4900dea731c8c765f89
Daniel Alexander Smith <daniel@danielsmith.eu>
2012-03-23 09:38:33 +0000

    Added gpl header

--- make_osx_app.sh
+++ make_osx_app.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+#    This file is part of WebBox.
+#
+#    Copyright 2011-2012 Daniel Alexander Smith
+#    Copyright 2011-2012 University of Southampton
+#
+#    WebBox is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # NOTE: This will not work unless you install python from source (I use homebrew to simplify this) - py2app does not ship the Apple-compiled python on purpose.
 
 # set up the library locations
