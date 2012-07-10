f2b769dbbe38ad407b022ec22278daaecca4d0f6
Daniel Alexander Smith <daniel@danielsmith.eu>
2012-07-10 10:31:52 +0100

    Fixed building of osx app

---
+++ build-dmg.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+# based on version from 4store
+
+$version=`git describe --tags --always | sed 's/^v//; s/-.*//'`;
+chomp $version;
+
+$du = `du -csk dist/WebBox.app`;
+($size) = ($du =~ /\s*(\d+)\s+total\s*$/si);
+$size = int $size * 1.5 / 1024 + 1;
+
+print("building $size MB DMG\n");
+
+$output = `hdiutil create "WebBox-$version" -ov -megabytes $size -fs HFS+ -volname \"WebBox-$version\"`;
+die "Couldn't create WebBox.dmg\nIs it mounted?" if $?;
+($dmgName) = ($output =~ /created\s*:\s*(.+?)\s*$/m);
+die "FATAL: Couldn't read created dmg name\n" unless $dmgName;
+print("INFO: dmgName is $dmgName\n");
+$output = `hdiutil attach \"$dmgName\"`;
+die "FATAL: Couldn't mount DMG $dmgName (Error: $?)\n" if $?;
+
+my ($dest) = ($output =~ /Apple_HFS\s+(.+?)\s*$/im);
+
+$output = `cp -r dist/WebBox.app "$dest"`;
+#$readme = `cat app-aux/README.rtf`;
+#$readme =~ s/\$\\{AV\\}/$version/g;
+#open(README, '>', "$dest/README.rtf");
+#print(README $readme);
+#close(README);
+system("ln -s /Applications \"$dest/Applications\"");
+
+$output = `hdiutil detach "$dest"`;
+die "FATAL: Error while copying files (Error: $err)\n" if $err;
+die "FATAL: Couldn't unmount device \"$dest\": $?\n" if $?;
+
+$tmpName = $dmgName."~";
+rename($dmgName, $tmpName);
+$output = `hdiutil convert "$tmpName" -format UDZO -imagekey zlib-level=8 -o "$dmgName"`;
+die "Error: Couldn't compress the dmg $dmgName: $?\n" if $?;
+unlink($tmpName);
+
+print("\nDONE: Final DMG in $dmgName\n");
--- make_osx_app.sh
+++ make_osx_app.sh
@@ -48,5 +48,5 @@ chmod +x dist/WebBox.app/Contents/Resources/4store/*
 # replace run with run.sh in the bundle Info.plist
 python scripts/change_app_run_script.py dist/WebBox.app/Contents/Info.plist
 
-echo "Now make a DMG of the right size, use AutoOpen to make it pop up, remove its toolbars, make the icons bigger, copy in the app, copy in a link to /Applications, resize, move to top left, and then zip."
+./build-dmg.sh
 
