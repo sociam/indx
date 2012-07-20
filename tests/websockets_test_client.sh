356516d939ce691a6924c4e2f8da0040870f3334
Daniel Alexander Smith <daniel@danielsmith.eu>
2012-07-20 18:54:10 +0100

    Rename test client script

---
+++ tests/websockets_test_client.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#    This file is part of WebBox.
+#
+#    Copyright 2011-2012 Daniel Alexander Smith
+#    Copyright 2011-2012 University of Southampton
+#
+#    WebBox is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
+
+# to change back to current dir
+export SSDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SSDIR"
+
+# run message broken, output to log
+source ../env/bin/activate
+#. scripts/config.sh
+
+python test_client.py
+
+
