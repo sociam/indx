29c92fb1615bcce538a4f72fda2102d8558d159d
Daniel Alexander Smith <daniel@danielsmith.eu>
2012-07-21 18:12:21 +0100

    Config script now finds home directory properly

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,12 +4,14 @@
 
 export PATH="${SECURESTORE_HOME}/4store:$PATH"
 export ME=`whoami`
+export HOME=`ls -d ~`
+
 
 export KBNAME="webbox_$ME"
 export RWW_JAR="${SECURESTORE_HOME}/rww/read-write-web.jar"
 export RWW_PORT="8213"
-export LOG_RWW="~/.webbox/data/logs/rww.log"
-export LOG_SECURESTORE="~/.webbox/data/logs/webbox_server.log"
-export LOG_4S="~/.webbox/data/logs/4store.log"
+export LOG_RWW="$HOME/.webbox/data/logs/rww.log"
+export LOG_SECURESTORE="$HOME/.webbox/data/logs/webbox_server.log"
+export LOG_4S="$HOME/.webbox/data/logs/4store.log"
 export PORT="8212"
-export RWW_LD="~/.webbox/data/rww"
+export RWW_LD="$HOME/.webbox/data/rww"
