fa8e08332a101601a99a4b127e26583a67f2e61c
Dr Daniel Alexander Smith <git@danielsmith.eu>
2012-10-23 18:34:59 +0100

    Added more benchmarking options

--- tests/objectstore/benchmark.py
+++ tests/objectstore/benchmark.py
@@ -1,4 +1,5 @@
 import psycopg2, time, sys
+import cProfile
 from objectstore import ObjectStore
 
 # database variables
@@ -9,8 +10,6 @@ db_name = "webbox_benchmark" # dropped at first
 db_user = "webbox"
 db_pass = "foobar"
 
-obj_count = 100 # how many objects to create
-prop_count = 100 # how many properties per object
 
 # double check
 drop_db = None
@@ -45,39 +44,68 @@ version = int(raw_input("Previous version (e.g. '0')? "))
 conn = psycopg2.connect(database=db_name, user=db_user, password=db_pass)
 store = ObjectStore(conn)
 
-store.autocommit(False)
 
-print "Preparing {0} benchmark objects with {1} properties each.".format(obj_count, prop_count)
-to_insert = []
-for i in range(obj_count):
+insert_test = raw_input("Perform INSERT test? [yes/no] ")
+if insert_test == "yes":
 
-    graph_path = "/webbox/object-{0}".format(i)
-    graph_uri = "http://localhost:8211{0}".format(graph_path)
+    store.autocommit(False)
 
-    obj = {"@id": graph_uri}
-    for j in range(prop_count):
-        propname = "{0}#prop{1}".format(graph_uri, j)
-        value = {"@value": "value-{0}".format(j)}
-        obj[propname] = [value]
+    obj_count = int(raw_input("Number of objects to insert: ")) # how many objects to create
+    prop_count = int(raw_input("Number of properties per object: ")) # how many properties per object
 
-    to_insert.append({"graph_uri": graph_uri, "objs": [obj]})
 
-def insert_all():
-    global to_insert
-    global store
+    print "Preparing {0} benchmark objects with {1} properties each.".format(obj_count, prop_count)
+    to_insert = []
+    uris = []
+    for i in range(obj_count):
 
-    print "Inserting objects..."
-    start_time = time.time()
-    for i in to_insert:
-        store.add(i['graph_uri'], i['objs'], version);
-    end_time = time.time()
-    total_time = end_time - start_time
+        graph_path = "/webbox/object-{0}".format(i)
+        graph_uri = "http://localhost:8211{0}".format(graph_path)
 
-    store.autocommit(True) # commits
+        obj_uri = "{0}-object".format(graph_uri)
 
-    print "Time taken: {0} seconds ({1} triples/second).".format(total_time, float(obj_count * prop_count) / float(total_time))
+        obj = {"@id": obj_uri}
+        uris.append({"graph": graph_uri, "object": obj_uri})
+        for j in range(prop_count):
+            propname = "{0}#prop{1}".format(graph_uri, j)
+            value = {"@value": "value-{0}".format(j)}
+            obj[propname] = [value]
+
+        to_insert.append({"graph_uri": graph_uri, "objs": [obj]})
+
+    def insert_all():
+        global to_insert
+        global store
+
+        print "Inserting objects..."
+        start_time = time.time()
+        for i in to_insert:
+            store.add(i['graph_uri'], i['objs'], version);
+        end_time = time.time()
+        total_time = end_time - start_time
+
+        store.autocommit(True) # commits
+        print "Time taken: {0} seconds ({1} triples/second).".format(total_time, float(obj_count * prop_count) / float(total_time))
     
 
-import cProfile
-cProfile.run('insert_all()')
+    cProfile.run('insert_all()')
+
+
+read_times = int(raw_input("Perform read test N times? [1] "))
+
+for i in range(read_times):
+    def read_all():
+        global uris
+        global store
+        print "Read all objects, one by one:"
+        start_time = time.time()
+        for item in uris:
+            graph_uri = item['graph']
+            object_uri = item['object']
+            store.get_latest_obj(graph_uri, object_uri)
+        end_time = time.time()
+        total_time = end_time - start_time
+        print "Time taken: {0} seconds ({1} objects/second).".format(total_time, float(len(uris)) / float(total_time))
+
+    cProfile.run("read_all()")
 
