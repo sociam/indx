a861320fed43d47a8f205566f334be357896f8d6
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2012-10-29 17:54:48 +0000

    Expand and compress language/datatype-free strings in objectstore.js

--- html/js/objectstore.js
+++ html/js/objectstore.js
@@ -59,15 +59,8 @@
                     break;
                 case "read":
                     var store = model;
-                    return ObjectStore.list_graphs(store, function(objects){
-                        store.reset(objects);
-                    });
-                    break;
+                    return ObjectStore.list_graphs(store);
                 case "update":
-                    /*
-                    console.debug("Update graph called on graph:",model);
-                    ObjectStore.update_graph(model);
-                    */
                     break;
                 case "delete":
                     break;
@@ -83,29 +76,25 @@
         objectstore_type: "graph",
         initialize: function(attributes){
         },
-        /*
-        fetch: function(){
-            // replaces graph.objects with a GraphCollection and calls the callback
-            var graph = this;
-            ObjectStore.fetch_graph(this, this.get("_store"), this.get("@id"), function(objects){
-                graph.set("objects", objects);
-            });
-        }
-        */
+        create: function(object_attrs){
+            // add a new object to this graph (ObjectStore.GraphCollection will create an Obj from this)
+            var clone = _.clone(object_attrs);
+            clone['_graph'] = this;
+
+            var model = new ObjectStore.Obj(clone);
+            this.get("objects").add(model);
+            return model;
+        },
         sync: function(method, model, options){
             switch(method){
                 case "create":
                     break;
                 case "read":
                     var graph = model;
-                    return ObjectStore.fetch_graph(model, model.get("_store"), model.get("@id"), function(objects){
-                        graph.set("objects", objects);
-                    });
-                    break;
+                    return ObjectStore.fetch_graph(model, model.get("_store"), model.get("@id"));
                 case "update":
                     console.debug("Update graph called on graph:",model);
                     return ObjectStore.update_graph(model);
-                    break;
                 case "delete":
                     break;
                 default:
@@ -136,7 +125,6 @@
                     // delegate to the graph
                     console.debug("Update to Obj: ",model);
                     return model.attributes._graph.sync("update", model.attributes._graph, options);
-                    break;
                 case "delete":
                     break;
                 default:
@@ -170,7 +158,7 @@
                     graphs.push(graph);
                 });
 
-                callback(graphs);
+                store.reset(graphs);
             }
         });
     };
@@ -188,7 +176,28 @@
                 if (pred[0] == "_"){
                     return;
                 }
-                out_obj[pred] = vals;
+                if (pred[0] == "@"){ // don't expand @id etc.
+                    out_obj[pred] = vals;
+                    return;
+                }
+
+                var obj_vals = [];
+
+                if (!(vals instanceof Array)){
+                    vals = [vals];
+                }
+                $.each(vals, function(){
+                    var val = this;
+                    if (typeof val === "string" || val instanceof String){
+                        obj_vals.push({"@value": val});
+                    } else if (val instanceof ObjectStore.Obj) {
+                        obj_vals.push({"@id": val.id });
+                    } else {
+                        obj_vals.push(val); // val is a fully expanded string, e.g. {"@value": "foo", "@language": "en" ... }
+                    }
+                });
+
+                out_obj[pred] = obj_vals;
             });
             graph_objs.push(out_obj);
         });
@@ -207,13 +216,12 @@
             type: "PUT",
             success: function(data){
                 // TODO check that it worked
-                // TODO record new version number
                 graph._version = data["@version"];
             }
         });
     };
 
-    ObjectStore.fetch_graph = function(graph, store, uri, callback){
+    ObjectStore.fetch_graph = function(graph, store, uri){
         // return a list of models (each of type ObjectStore.Object) to populate a GraphCollection
         return $.ajax({
             url: store.server_url,
@@ -221,9 +229,12 @@
             dataType: "json",
             type: "GET",
             success: function(data){
+                var graph_collection = new ObjectStore.GraphCollection();
+                graph_collection._store = store;
 
-                var objs = [];
-                var version = -1;
+                graph.set("objects", graph_collection); 
+
+                var version = 0; // new version FIXME check
                 $.each(data, function(uri, obj){
                     if (uri == "@version"){
                         version = obj;
@@ -232,19 +243,32 @@
                         // ignore @graph etc.
                         return;
                     }
-                    if (!("@id" in obj)){
-                        obj["@id"] = uri;
-                    }
-                    obj._graph = graph;
-                    objs.push(obj);
-                });
 
-                var graph_collection = new ObjectStore.GraphCollection(objs);
-                graph_collection._store = store;
+                    var obj_model = {"@id": uri, "_graph": graph};
+
+                    $.each(obj, function(key, vals){
+                        var obj_vals = [];
+                        $.each(vals, function(){
+                            var val = this;
+                            if ("@id" in val){
+                                var val_obj = graph.create({"@id": val["@id"]});
+                                obj_vals.push(val_obj);
+                            } else if ("@value" in val){
+                                // if the string has no language or datatype, compress to just a string.
+                                if ("@language" in val && val["@language"] == "" && "@type" in val && val["@type"] == ""){
+                                    obj_vals.push(val["@value"]);
+                                } else {
+                                    obj_vals.push(val);
+                                }
+                            }
+                        });
+                        obj_model[key] = obj_vals;
+                    });
+
+                    graph.create(obj_model);
+                });
                 graph_collection._version = version;
                 graph._version = version;
-
-                callback(graph_collection);
             }
         });
     };
