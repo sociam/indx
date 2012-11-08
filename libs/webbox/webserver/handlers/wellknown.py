70385f8101dfae3a7535b08bec86ef0d95ec0e60
Dr. Daniel Alexander Smith <git@danielsmith.eu>
2012-11-08 20:46:44 +0000

    Refactor wellknown handler

--- libs/webbox/webserver/handlers/wellknown.py
+++ libs/webbox/webserver/handlers/wellknown.py
@@ -21,43 +21,32 @@ from urlparse import urlparse, parse_qs
 
 class WellKnownHandler:
 
-    def __init__(self, server_url):
-        self.server_url = server_url
+    base_path = '.well-known'
+    subhandlers = {
+        'host-meta': {
+            'methods': ['GET'],
+            'require_auth': False,
+            'require_token': False,
+            'handler': WellKnownHandler.host_meta_xml,
+            'content-type': 'application/xrd+xml; charset=UTF-8'
+        },
+        'host-meta.json': {
+            'methods': ['GET'],
+            'require_auth': False,
+            'require_token': False,
+            'handler': WellKnownHandler.host_meta_json,
+            'content-type': 'application/json; charset=UTF-8'
+        },
+    }
 
-    def response_well_known(self, environ, start_response):
-        """ WSGI response handler for /.well-known/ ."""
-        logging.debug("Calling WebBox .well-known response(): " + str(environ))
-        try:
-            req_type = environ['REQUEST_METHOD']
+    def host_meta_xml(self, request):
+        resource = request.args['resource'][0]
 
-            if "REQUEST_URI" in environ:
-                url = urlparse(environ['REQUEST_URI'])
-                req_path = url.path
-                req_qs = parse_qs(url.query)
-            else:
-                req_path = environ['PATH_INFO']
-                req_qs = parse_qs(environ['QUERY_STRING'])
+        subject = ""
+        if resource is not None:
+            subject = "<Subject>%s</Subject>" % (resource)
 
-            headers = []
-
-            if req_type == "GET":
-                # handle the /.well-known/ call
-
-                if len(req_path) > 0 and req_path[0] == "/":
-                    req_path = req_path[1:] # strip / from start of path
-
-                resource = None
-                if "resource" in req_qs:
-                    resource = req_qs['resource'][0]
-
-                if req_path == "host-meta":
-                    headers.append( ("Content-Type", "application/xrd+xml; charset=UTF-8") )
-
-                    subject = ""
-                    if resource is not None:
-                        subject = "<Subject>%s</Subject>" % (resource)
-
-                    response = """<?xml version='1.0' encoding='UTF-8'?>
+        response = """<?xml version='1.0' encoding='UTF-8'?>
 <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0' 
      xmlns:hm='http://host-meta.net/xrd/1.0'>
 
@@ -74,52 +63,34 @@ class WellKnownHandler:
         <Property type='auth-method'>https://tools.ietf.org/html/draft-ietf-oauth-v2-26#section-4.2</Property>
   </Link>
 </XRD>
-""" % (subject, self.server_url)
+""" % (subject, self.webbox.server_url)
 
+        request.write(response)
+        request.finish()
 
-                elif req_path == "host-meta.json":
-                    headers.append( ("Content-Type", "application/json; charset=UTF-8") )
+    def host_meta_json(self, request):
+        resource = request.args['resource'][0]
 
-                    response_json = {"links": [
-                        { "rel": "lrdd",
-                          "template": "/lrdd?uri={uri}",
-                        },
-                        {
-                          "rel": "remoteStorage",
-                          "href": self.server_url,
-                          "type": "https://www.w3.org/community/rww/wiki/read-write-web-00#simple",
-                          "properties": {
-                              'auth-method': "https://tools.ietf.org/html/draft-ietf-oauth-v2-26#section-4.2",
-                              'auth-endpoint': "/openid",
-                          }
-                        }
-                    ]}
+        response_json = {"links": [
+            { "rel": "lrdd",
+              "template": "/lrdd?uri={uri}",
+            },
+            {
+              "rel": "remoteStorage",
+              "href": self.webbox.server_url,
+              "type": "https://www.w3.org/community/rww/wiki/read-write-web-00#simple",
+              "properties": {
+                  'auth-method': "https://tools.ietf.org/html/draft-ietf-oauth-v2-26#section-4.2",
+                  'auth-endpoint': "/openid",
+              }
+            }
+        ]}
 
-                    if resource is not None:
-                        response_json['subject'] = resource
+        if resource is not None:
+            response_json['subject'] = resource
 
-                    response = json.dumps(response_json, indent=2)
+        response = json.dumps(response_json, indent=2)
 
-                else:
-                    start_response("404 Not Found", [])
-                    return [""]
+        request.write(response)
+        request.finish()
 
-            else:
-                # When you sent 405 Method Not Allowed, you must specify which methods are allowed
-                start_response("405 Method Not Allowed", [("Allow", "GET"), ("Allow", "OPTIONS")])
-                return [""]
-        
-            # add CORS headers (blanket allow, for now)
-            headers.append( ("Access-Control-Allow-Origin", "*") )
-            headers.append( ("Access-Control-Allow-Methods", "POST, GET, PUT, HEAD, OPTIONS") )
-            headers.append( ("Access-Control-Allow-Headers", "Content-Type, origin, accept, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control") )
-
-            response = response.encode("utf8")
-            headers.append( ("Content-length", len(response)) )
-            start_response("200 OK", headers)
-            return [response]
-
-        except Exception as e:
-            logging.debug("Error in WebBox.response_well_known(), returning 500: %s, exception is: %s" % (str(e), traceback.format_exc()))
-            start_response("500 Internal Server Error", [])
-            return [""]
