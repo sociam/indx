e550a6d9b58a72cf223856a14974d27fa9f4f172
electronic max <electronic@gmail.com>
2012-11-14 21:30:00 +0000

    forgot to add tokens

---
+++ libs/webbox/webbox_pg2.py
@@ -0,0 +1,56 @@
+
+
+
+import logging, urllib2, uuid, rdflib, os, traceback, mimetypes, shutil, json
+import psycopg2
+from txpostgres import txpostgres
+from twisted.internet.defer import Deferred
+from time import strftime
+
+## webbox to txpostgres bridge
+
+WBPREFIX = "wb_"
+
+def connect(db_name,db_user,db_pass):
+    conn = txpostgres.Connection()
+    if db_name is None:
+        conn_str = ("user='{0}' password='{1}'".format(db_user, db_pass))
+    else:
+        conn_str = ("dbname='{0}' user='{1}' password='{2}'".format(db_name, db_user, db_pass))
+    return conn.connect(conn_str)
+
+def connect_box(box_name,db_user,db_pass):
+    return connect(WBPREFIX + box_name, db_user, db_pass)
+
+def auth(db_user,db_pass):
+    d = Deferred()
+    connect(None,db_user,db_pass).addCallbacks(lambda *x: d.callback(True), lambda *x: d.callback(False))
+    return d
+
+def create_box(box_name, db_user, db_pass):
+    db_name = WBPREFIX + box_name
+    root_conn = psycopg2.connect(user = db_user, password = db_pass)
+    root_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    root_cur = root_conn.cursor()
+    root_cur.execute("CREATE DATABASE %s WITH ENCODING='UTF8' OWNER=%s CONNECTION LIMIT=-1" % (db_name, db_user))
+    root_conn.commit()
+    root_cur.close()
+    root_conn.close()    
+
+    # load in definition from data/objectstore.sql
+    fh_objsql = open(os.path.join(os.path.dirname(__file__),"..","..","data","objectstore.sql")) # FIXME put into config
+    objsql = fh_objsql.read()
+    fh_objsql.close()
+
+    root_conn = psycopg2.connect(database = db_name, user = db_user, password = db_pass) # reconnect to this new db, and without the isolation level set
+    root_cur = root_conn.cursor()
+    root_cur.execute(objsql)
+    root_conn.commit()
+    root_cur.close()
+    root_conn.close()
+    return
+
+def create_role():
+    
+    return 
+
---
+++ libs/webbox/webserver/token.py
@@ -0,0 +1,60 @@
+#    This file is part of WebBox.
+#
+#    Copyright 2012 Daniel Alexander Smith, eMax
+#    Copyright 2012 University of Southampton
+#
+#    WebBox is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WebBox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, logging, time, traceback, json, re, uuid
+from urlparse import urlparse, parse_qs
+from twisted.web import script
+from twisted.internet import reactor
+from twisted.web.resource import ForbiddenResource, Resource
+from twisted.web.server import Site
+from twisted.web.util import Redirect
+from twisted.web.static import File, Registry
+from twisted.web.wsgi import WSGIResource
+from twisted.internet import reactor, ssl
+from twisted.internet.defer import Deferred
+
+class Token:
+    def __init__(self, username, password, boxid, appid, origin, conn):
+        self.username = username
+        self.password = password
+        self.boxid = boxid
+        self.appid = appid
+        self.origin = origin
+        self.conn = conn
+        self.id = str(uuid.uuid1())
+
+class TokenKeeper:
+    # handles token garbage collection at some time in the future!
+    def __init__(self):
+        self.tokens = {}
+    def get(self,tid):
+        return self.tokens.get(tid)
+    def add(self,token):
+        self.tokens[token.id] = token
+        return token
+    def new(self,username,password,boxid,appid,origin,conn):
+        token = Token(username,password,boxid,appid,origin,conn)
+        self.add(token)
+        return token
+
+    
+    
+    
+        
+    
+        
