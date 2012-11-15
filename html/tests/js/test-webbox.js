9b8a02dba49048b6d4183a7d3be87a1e7eb4b516
electronic max <electronic@gmail.com>
2012-11-15 01:27:09 +0000

    tests

--- html/tests/js/test-webbox.js
+++ html/tests/js/test-webbox.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
 	console.log('http://'+host+':8211/js/webbox-backbone.js');
 	$.getScript('http://'+host+':8211/js/webbox-backbone.js', function() {
 		console.log('get script done');
-	  	var store = new ObjectStore.Store([], { server_url : "http://"+document.location.host+":8211/" });
+	  	var store = new ObjectStore.Store([], { server_url : "http://"+host+":8211/" });
 		window.store = store;
 		$('#login').click(function() {
-			console.log('logging in as user ', $('#username').val())
 			var username = $('#username').val(), pass = $('#password').val()
-			store.login(username,password).then(function() {
+			console.log('logging in as user ', username, pass)			
+			store.login(username,pass).then(function() {
 				console.log('logged in ');
 			}).fail(function(err) {
 				console.log('fail ', err);
