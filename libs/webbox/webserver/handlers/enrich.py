22add278d0846067a8db7098f4bd2f004215253e
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2012-11-17 23:21:29 +0000

    Autocomplete places

--- libs/webbox/webserver/handlers/enrich.py
+++ libs/webbox/webserver/handlers/enrich.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging, traceback, json
+import logging, traceback, json, os
 from twisted.web.resource import Resource
 from twisted.internet.defer import Deferred
 
