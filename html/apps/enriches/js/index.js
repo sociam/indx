1fc17a5725f6f8af228a6780179ad065c2791f21
Colin Williams <github@crwilliams.co.uk>
2012-11-18 13:54:40 +0000

    Added statement on #list.

--- html/apps/enriches/js/index.js
+++ html/apps/enriches/js/index.js
@@ -11,6 +11,23 @@ $(document).ready(function() {
 	console.log('setting baseurl to ', document.location.pathname, '-', basepath);
 	require.config({ baseUrl:  basepath });
 	require(['apps/enriches/js/enrich-app', 'components/toolbar/toolbar'], function(enrich, tbcomponent) {
+	    function populate_table(box) {
+	        box.ajax('/get_all_transactions', 'GET', { 'persona': 'Persona2' })
+				.then(function(results) {
+				    results['data'].map(function(result) {
+				        var place = '';
+				        var establishment = '';
+				        if(result['place_id']) {
+				            place = result['place_id'];
+			            }
+    				    if(result['establishment_id']) {
+    				        establishment = result['establishment_id'];
+				        }
+				        $('<tr><td>'+result['date']+'</td><td>'+result['description']+'</td><td>'+result['amount']+'</td><td>'+establishment+'</td><td>'+place+'</td></tr>').appendTo($('#tran-list table'));
+			        });
+				});
+	    }
+	    
 		$.getScript('http://'+host+':8211/js/webbox-backbone.js', function() {
 	  		var store = new ObjectStore.Store();
 			window.store = store;
@@ -34,14 +51,17 @@ $(document).ready(function() {
 				store.create_box(boxname).then(load_box).fail(load_box);
 				return d.promise();
 			};
+			var init_app = true;
 			toolbar.on('change:box', function(b) {
 				console.log('change box ', b);
 				if (b !== undefined) {					
 					// enrich.set({box:b});
 					get_graph(b).then(function(box,graph) {
-						if (app === undefined) {
+						if (app === undefined && init_app) {
 							console.log("LOADING with BOX ", box.id);
-							app = enrich.init(box);
+    						app = enrich.init(box);
+						} else {
+						    populate_table(box);
 						}
 						// enrich.set({graph:graph});
 					});
@@ -50,6 +70,15 @@ $(document).ready(function() {
 			toolbar.on('login', function() { if (app !== undefined) { app.show(); }	});			
 			toolbar.on('logout', function() { if (app !== undefined) { app.hide(); }});
 			// app = enrich.init();
+			
+			if(document.location.hash == '#list') {
+			    init_app = false;
+			    $('.round-holder, .save').hide();
+			    $('<div id="tran-list"></div>').appendTo('body');
+			    $('#tran-list').append('<table></table>');
+			    
+			}
+			
 			window.app = app;
 		});
 	});
