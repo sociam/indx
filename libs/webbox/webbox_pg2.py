5d1a6398454a2fa5cb88d729782e790e7cb35fb2
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-03-10 17:42:09 +0000

    Remove unused imports, and add license blurb

--- libs/webbox/webbox_pg2.py
+++ libs/webbox/webbox_pg2.py
@@ -1,11 +1,25 @@
+#    This file is part of WebBox.
+#
+#    Copyright 2012-2013 Daniel Alexander Smith, Max Van Kleek
+#    Copyright 2012-2013 University of Southampton
+#
+#    WebBox is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    WebBox is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
 
-
-
-import logging, urllib2, uuid, os, traceback, mimetypes, shutil, json
+import os
 import psycopg2
 from txpostgres import txpostgres
 from twisted.internet.defer import Deferred
-from time import strftime
 
 ## webbox to txpostgres bridge
 
