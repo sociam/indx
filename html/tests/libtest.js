9384c141aca93dd1b2dde67ccdff3a4660c5703a
electronic max <electronic@gmail.com>
2013-03-11 01:05:24 +0000

    changed libtest slightly

--- html/tests/libtest.js
+++ html/tests/libtest.js
@@ -6,15 +6,13 @@ WebBox.load().then(function(exports) {
 	$('#loader').fadeOut('slow', function() {
 		$('.loaded').slideDown('slow');
 		window.store = new WebBox.Store();
-		store.login('electronic','foo').then(function() {
-			console.log('log in ok');
-			store.toolbar.on('change:selected-box', function(b) {
-				console.log('change:selected-box ----------------- ', b, store.boxes().get(b));
-				var box = store.boxes().get(b);
-				box.fetch().then(function(box) {
-					console.log('loaded --- ', box);
-				});
+		store.on('login', function() { console.log('log in ok'); });
+		store.toolbar.on('change:selected-box', function(b) {
+			console.log('change:selected-box ----------------- ', b, store.boxes().get(b));
+			var box = store.boxes().get(b);
+			box.fetch().then(function(box) {
+				console.log('loaded --- ', box);
 			});
-		});			
+		});
 	});
 });
