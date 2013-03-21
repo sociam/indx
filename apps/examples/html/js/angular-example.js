9d10661d5755a525808e8636121bc076ea38f3bc
electronic Max <electronic@gmail.com>
2013-03-20 21:21:41 -0700

    major simplification to angular example

--- apps/examples/html/js/angular-example.js
+++ apps/examples/html/js/angular-example.js
@@ -15,40 +15,15 @@ function AppController($scope) {
 			var next = function() {
 				box.get_obj('example1').then(function(o) {
 					model = o;
-					var lastmodelval, lastinputval;
-
-					
-					$scope.$watch('inputmodel', function() {
-						var val = $scope.inputmodel;
-
-						// prevent our incoming modifications from causing us to trigger
-						if (val === lastmodelval) {
-							// console.log('viewchange :: skipping propagate ');
-							return;
-						}
-						
-						// silent updating
-						if (model.get('value') !== undefined &&	val === model.get('value')[0]) {
-							// console.log('value still the same');
-							return;
-						}						
-						
-						// console.log('view -> model. SAVING ', val);
-						lastinputval = val;
-						model.set('value', $scope.inputmodel);  // this is going to cause a trigger
+					$scope.update = function() {
+						model.set({value: $scope.inputmodel}, {silent:true});
 						try { model.save(); } catch(err) { console.error(err); }
-					});
+					};
 
 					var update_view = function() {
 						var val = model.get('value') !== undefined ? model.get('value')[0] : undefined;
-						if (val === lastinputval) {
-							// console.log('modelchange :: skipping propagate');
-							return;
-						}
 						if (val === $scope.inputmodel) { return; }
-						// console.log('model -> VIEW ', val);		
-						lastmodelval = val;
-						$scope.$apply(function() {	$scope.inputmodel = val;	});
+						$scope.$apply(function() {	$scope.inputmodel = val; });
 					};
 					model.on('change:value', update_view);
 					update_view();
