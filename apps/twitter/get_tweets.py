540a02133d139523961537a98d921ab03d51522c
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-04-23 13:28:33 +0100

    Remove unused import

--- apps/twitter/get_tweets.py
+++ apps/twitter/get_tweets.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with WebBox.  If not, see <http://www.gnu.org/licenses/>.
 
-import tweetstream, argparse, logging, getpass, sys, urllib, urllib2, json
+import tweetstream, argparse, logging, getpass, sys, urllib2, json
 from pywebbox import WebBox
 
 
