fe75fccbe6ed257fee46171ecff0cd7458490e81
electronic Max <electronic@gmail.com>
2013-05-19 21:41:12 +0100

    timeseries with property values

--- html/components/timeseries/timeseries.js
+++ html/components/timeseries/timeseries.js
@@ -7,7 +7,7 @@
 		.directive('timeseries', function() {
 			return {
 				restrict: 'E',
-				scope:{model:"=model", box:"=box"}, // these means the attribute 'm' has the name of the scope variable to use
+				scope:{model:"=model", box:"=box", property:"=property"}, // these means the attribute 'm' has the name of the scope variable to use
 				templateUrl:'/components/timeseries/timeseries.html',
 				controller:function($scope, $element, $attrs, webbox) {
 					// incoming :
@@ -107,15 +107,18 @@
 					var update = function() {
 						try {
 							// run from inside safe apply
-							var ps = $attrs.properties.split(',').map(function(x) { return x.trim(); });
-							$scope.hello = ' '+ ps.map(function(p) {
-								var v = $scope.model.get(p);
-								return p + ": " + (v ? v.toString() : 'none');
-							}).join(',');
-                            console.debug("element",$element);
-                            update_chart($attrs, $element);
-//                            $scope.chart = update_chart($attrs);
-							console.log('TIME SERIES UPDATE > ', $scope.hello);
+							console.log('property ', $scope.property);
+							update_chart($element, $scope.model.get($scope.property));
+														
+							// var ps = $attrs.property.split(',').map(function(x) { return x.trim(); });
+// 							$scope.hello = ' '+ ps.map(function(p) {
+// 								var v = $scope.model.get(p);
+// 								return p + ": " + (v ? v.toString() : 'none');
+// 							}).join(',');
+//                             console.debug("element",$element);
+//                             update_chart($attrs, $element);
+// //                            $scope.chart = update_chart($attrs);
+// 							console.log('TIME SERIES UPDATE > ', $scope.hello);
 							
 						} catch(e) {
 							console.error(e);
