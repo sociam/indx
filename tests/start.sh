f0f9edb54f863379546da8768b8aebef4785f625
Peter <peter-XPS-L521X>
2013-06-25 15:17:30 +0100

    Configured Yeti, but doesn't seem to support e2e testing or proxy

--- tests/start.sh
+++ tests/start.sh
@@ -2,6 +2,6 @@
 
 ./node_modules/.bin/yeti --server &
 
-while inotifywait -r -e modify test/*.html; do
-	./node_modules/.bin/yeti test/*.html
+while inotifywait -qr -e modify test/*.html; do
+	./node_modules/.bin/yeti http://localhost:8211/apps/tests/angular-example.html
 done
