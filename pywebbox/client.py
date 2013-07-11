e4b4b04bff65c18cfe4d756077bcdfb39406a2ae
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-07-11 15:33:00 +0100

    WebBox to INDX in license headers

--- pywebbox/client.py
+++ pywebbox/client.py
@@ -3,7 +3,7 @@
 #    Copyright 2013 Daniel Alexander Smith
 #    Copyright 2013 University of Southampton
 #
-#    WebBox is free software: you can redistribute it and/or modify
+#    INDX is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
