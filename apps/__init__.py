59e4e193ea41d05229f2748743e9783d68d8dabf
Dr Daniel Alexander Smith <daniel@danielsmith.eu>
2013-07-31 13:54:52 +0100

    Handle application erroring to not break the server

--- apps/__init__.py
+++ apps/__init__.py
@@ -13,7 +13,13 @@ def find_html_dirs():
     subdirs = [(o,os.path.sep.join([curdir,o,'html'])) for o in os.listdir(curdir) if os.path.exists(os.path.sep.join([curdir,o,'html']))]
     return dict(subdirs)
 
+def import_app(app):
+    try:
+        importlib.import_module(app)
+    except Exception as e:
+        logging.error("Couldn't load app: {0}, error: {1}".format(app, e))
+
 MODULES = {}
 _html_dirs = find_html_dirs()
-[ MODULES.update({m_name:{'module':importlib.import_module('.'.join(['apps',m_name])), 'html':_html_dirs.get(m_name)}}) for m_name in find_module_dirs() ]
+[ MODULES.update({m_name:{'module': import_app('.'.join(['apps',m_name])), 'html':_html_dirs.get(m_name)}}) for m_name in find_module_dirs() ]
 
