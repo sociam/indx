a796f8c71d53ac73d768374efc92605343337f95
Peter West <peter.g.west@gmail.com>
2013-08-29 16:58:26 +0100

    Work on todos

--- apps/todos/html/js/todos.js
+++ apps/todos/html/js/todos.js
@@ -7,40 +7,28 @@ angular
 		var box,
 			u = utils;
 
-		$scope.loading = 0;
-
-		var todos_sorted_by_time = function (todos) {
-			todos = todos.concat([]); // clone
-			todos.sort(function (x, y) {
-				return y.get('timestamp')[0] - x.get('timestamp')[0];
-			});
-			return todos;
-		};
 
 		// utilities used by html5
-		$scope.to_date_string = function (d) {
-			return (new Date(d)).toDateString();
-		};
-		$scope.to_time_string = function (d) {
-			return (new Date(d)).toLocaleTimeString();
-		};
+		var to_date_string = function (d) { return (new Date(d)).toDateString(); };
+		var to_time_string = function (d) { return (new Date(d)).toLocaleTimeString(); };
 
 		// called by the refresh button
-		$scope.create_todo = function() {
-			$scope.loading++;
-			$scope.error = undefined;
+		var create_todo = function() {
 			var title = $('#todo_title').val();
 
 			u.safe_apply($scope, function() {
-				$scope.loading--;
-				u.debug('todo >> ', title);
-				u.safe_apply($scope, function() {
-					$scope.add_todo({ title: title });
+				add_todo({ title: title }).then(function (todo) {
+					u.safe_apply($scope, function() {
+						edit_todo(todo);
+					});
 				});
 			});
 		};
 
-		$scope.add_todo = function (todo) {
+
+
+		var add_todo = function (todo) {
+			var promise = $.Deferred();
 			console.log('add todo');
 			if (!box) { u.debug('no box, skipping '); return ;}
 			box.get_obj('my_todo_list').then(function (todo_list) {
@@ -62,41 +50,54 @@ angular
 						.then(function() {
 							console.log('todo list save >> !!!!!!!!!!!!!!!!!!!!! ');
 							todo_list.save()
-								.then(function () { u.debug('updated todo list' + todo_list_items.length); })
+								.then(function () {
+									u.debug('updated todo list' + todo_list_items.length);
+									promise.resolve(o);
+								})
 								.fail(function () { u.error('could not update todo list '); });
 						}).fail(function(e) { u.error('could not create new todo ' + e); });
 				});
 			});
+			return promise;
 		};
 
-		$scope.edit_todo = function (todo) {
-			if ($scope.editing_todo) { $scope.editing_todo.editing = false; }
-			$scope.editing_todo = todo;
-			todo.newAttributes = _.clone(todo.attributes);
-			if (todo.newAttributes.urgency.length) {
-				todo.newAttributes.urgency = todo.newAttributes.urgency[0]; /// YEAHHHHHHH!!!!!
+		var edit_todo = function (todo) {
+			if (todo) {
+				$scope.editing_todo = todo;
+				todo.newAttributes = _.clone(todo.attributes);
+				if (todo.newAttributes.urgency.length) {
+					todo.newAttributes.urgency = todo.newAttributes.urgency[0]; /// YEAHHHHHHH!!!!!
+				}
+			} else {
+				$scope.editing_todo = undefined;
 			}
-			if ($scope.editing_todo) { $scope.editing_todo.editing = true; }
 		};
 
-		$scope.save_todo = function (todo) {
+		var is_editing = function (todo) {
+			return $scope.editing_todo && todo.id === $scope.editing_todo.id;
+		};
+
+		var save_todo = function (todo) {
 			todo.save(todo.newAttributes);
 			$scope.edit_todo();
 		};
 
-		$scope.toggle_todo = function (todo) {
-			todo.save('completed', !todo.get('completed') || !todo.get('completed')[0]);
+		var toggle_todo = function (todo) {
+			todo.save('completed', !pop(todo.get('completed')));
 		};
 
 		var update_todo_list_view = function(todo_list) {
 			var todos = set_defaults(todo_list.get('todos') || []),
 				groups = _.map(['For today', 'For another time'], function (group_title, i) {
-					var today = i === 0 ? true : false;
+					var today = i === 0 ? true : false,
+						filtered_todos = _.select(todos, function (todo) {
+							var t = pop(todo.get('today'));
+							return today ? t === true : t !== true;
+						});
 					return {
 						title: group_title,
-						by_time: _.select(todos_sorted_by_time(todos), function (todo) {
-							return today ? todo.get('today') === true : todo.get('today') !== true;
-						})
+						todos: filtered_todos,
+						sorted_by: get_sorted_todo_lists(filtered_todos)
 					};
 				});
 
@@ -107,10 +108,9 @@ angular
 		};
 
 		var update_todo_list_watcher = function() {
-			$scope.by_time = [];
 			if (!box) { u.debug('no box, skipping '); return ;}
 			box.get_obj('my_todo_list').then(function (todo_list) {
-				console.log('todo list >> ', todo_list, todo_list.get('todos'));
+				console.log('todo list >> ', todo_list.toJSON(), todo_list.get('todos'));
 				todo_list.on('change', function() {
 					console.log('todo list changed !! ');
 					update_todo_list_view(todo_list);
@@ -121,9 +121,45 @@ angular
 
 		var set_defaults = function (todos) {
 			return _.map(todos, function (todo) {
-				if (!todo.get('urgency')) { todo.set('urgency', 'low'); }
+				//if (!todo.get('urgency')) { todo.set('urgency', 'low'); }
+				//if (!todo.get('title')) { todo.set('title', ''); }
 				return todo;
 			});
+		};
+
+		var sorters = [
+			{	key: 'priority',
+				name: 'Priority',
+				sort: function (todo) { return -priority_number(pop(todo.get('urgency'))); } },
+			{	key: 'alphabetical',
+				name: 'Alphabetical',
+				sort: function (todo) { return pop(todo.get('title')); } },
+			{	key: 'date',
+				name: 'Date',
+				sort: function (todo) { return pop(todo.get('timestamp')); } }
+		];
+
+		var get_sorted_todo_lists = function (todos) {
+			var o = {};
+			_.map(sorters, function (sorter) {
+				o[sorter.key] = _.sortBy(todos, sorter.sort);
+			});
+			return o;
+		};
+
+		var todos_sorted_by_time = function (todos) {
+			return _.sortBy(todos, sorters.date.sort);
+		};
+
+		var pop = function (arr) {
+			if (!_.isArray(arr)) { return arr; }
+			return _.first(arr);
+		};
+
+		var priority_number = function (priority) {
+			return priority === 'low' ? 0 :
+				priority === 'med' ? 1 :
+				priority === 'high' ? 2 : 3;
 		};
 
 		// watches the login stts for changes
@@ -137,7 +173,19 @@ angular
 			}
 		});
 
-		window.s = client.store;
-
+		_.extend($scope, {
+			to_date_string: to_date_string,
+			to_time_string: to_time_string,
+			create_todo: create_todo,
+			add_todo: add_todo,
+			edit_todo: edit_todo,
+			save_todo: save_todo,
+			toggle_todo: toggle_todo,
+			editing_todo: undefined,
+			is_editing: is_editing,
+			pop: pop,
+			sorters: sorters,
+			todo_sorter: { key: sorters[0].key, asc: true }
+		});
 
 	});
