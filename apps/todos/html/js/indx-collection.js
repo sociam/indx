ba2b720fef024af8d8221412ed184a0bb19d2b8f
Peter West <peter.g.west@gmail.com>
2013-09-04 15:53:13 +0100

    taken out backbone stuff as module

---
+++ apps/todos/html/js/indx-collection.js
@@ -0,0 +1,237 @@
+/* global console, Backbone, _, $ */
+angular
+	.module('indx')
+	.factory('collection', function () {
+		'use strict';
+
+		var Model = function () {};
+		Model.extend = function (obj) {
+			var F = function () {};
+			F.prototype = _.extend({}, Model.prototype, obj);
+			return F;
+		};
+		_.extend(Model.prototype, {
+			create: function () {
+				var that = this;
+				console.log('creating item');
+				this.box.get_obj(this.id).then(function (new_obj) {
+					console.log('new item');
+					new_obj.save(that.newAttributes).then(function () {
+						console.log('saved');
+						that.trigger('created', new_obj);
+					});
+				});
+				return this;
+			},
+			remove: function () {
+				console.log('remove item', this);
+				this.destroy().then(function () {
+					console.log('destroyed item');
+					//that.trigger('destroyed', this);
+				});
+			},
+			edit: function (is_new) {
+				console.log('edit item', this);
+				this.newAttributes = _.clone(this.attributes);
+				this.is_new = is_new;
+				this.is_editing = true;
+				return this;
+			},
+			stage_and_save: function () {
+				var that = this;
+				console.log('stage and save');
+				if (this.is_new) {
+					console.log('new, so create');
+					return this.create();
+				} else {
+					this.save(this.newAttributes).then(function () {
+						console.log('saved');
+						that.restore();
+						// u.safe_apply($scope); TODO
+					});
+				}
+			},
+			save: function () {
+				if (this.is_new) {
+					console.warn('supressing save');
+				} else {
+					return Backbone.Model.prototype.save.apply(this, arguments);
+				}
+			},
+			restore: function () {
+				this.is_editing = false;
+				this.trigger('restore');
+			},
+			select: function (selected, options) {
+				options = options || {};
+				console.log('select', selected);
+				selected = _.isBoolean(selected) ? selected : true;
+				if (this.selected !== selected) {
+					console.log('selecting', selected);
+					this.selected = selected;
+					this.trigger('select', selected, options);
+				}
+			},
+			is_editing: false,
+			is_new: false
+		});
+		var Collection = Backbone.Collection.extend({
+			model: Model,
+			selected: undefined,
+			initialize: function (models, options) {
+				var that = this;
+				this.options = options || {};
+				this.box = this.options.box;
+				this.obj = this.options.obj;
+				this.on('add remove', function () {
+					console.log('something in the collection has changed');
+					that.save();
+				}).on('add remove reset', function () {
+					that.set_new_model(that._new_model);
+				});
+				this.reset(models);
+				this.populate();
+				this.set_new_model();
+			},
+			new_model: function (attributes, options) {
+				var that = this,
+					model = new Backbone.Model(attributes);
+				options = options || {};
+				this.extend_model(model);
+				model
+					.edit(true)
+					.set({ timestamp: now() })
+					.on('created', function (model) {
+						console.log('going for the add');
+						that.add(model);
+						that.set_new_model();
+						if (options.select) { model.select(); }
+					})
+					.on('restore', function () {
+						that.set_new_model();
+						if (options.select) { that.selected = undefined; }
+					});
+				this.set_new_model(model);
+				if (options.select) { model.select(true, { save: false }); }
+				return model;
+			},
+			set_new_model: function (model) {
+				this._new_model = model;
+				this.all_models = model ? this.models.concat([ model ]) : this.models;
+				//console.log('update');
+				this.trigger('update', this);
+			},
+			remove: function (model) {
+				console.log('!!!remove!!!', arguments);
+				if (this.selected === model) { this.selected = undefined; }
+				return Backbone.Collection.prototype.remove.apply(this, arguments);
+			},
+			extend_model: function (model) {
+				var that = this,
+					prototype = this.model.prototype;
+				_.extend(model, prototype);
+				if (prototype.initialize) {
+					prototype.initialize.apply(model);
+				}
+				if (prototype.defaults) {
+					_.defaults(model.attributes, prototype.defaults);
+				}
+				model.box = this.box;
+				model.on('select', function (selected, options) {
+					options = options || {};
+					if (selected && that.selected === model) { return; }
+					if (that.selected) { that.selected.select(false); }
+					if (selected) {
+						that.selected = model;
+						that.trigger('select', model);
+					}
+					if (options.save !== false &&
+							(that.options.saveSelected || this.options.syncSelected)) {
+						model.save({ selected: selected });
+						that.each(function (m) {
+							if (m !== model && pop(m.get('selected'))) {
+								m.save({ selected: false });
+							}
+						});
+					}
+				}).on('restore', function () {
+					that.trigger('update');
+				});
+				/*
+				model.on('destroyed', function () { // TODO: unbind to prevent memory leaks
+					console.log('detected destroy, removing model');
+					that.remove(model);
+				});*/
+			},
+			add: function (model, options) {
+				var that = this;
+				if (_.isArray(model)) {
+					_.each(model, function (model) { that.add(model, options); });
+				} else {
+					if (!_.isObject(model)) {
+						console.warn('There was a non-object stored??', model);
+						return;
+					}
+					this.extend_model(model);
+					if (this.options.saveSelected || this.options.syncSelected) {
+						if (pop(model.get('selected')) === true) {
+							model.select();
+						}
+						if (this.options.syncSelected) {
+							model.on('change:selected', function () {
+								model.select(pop(model.get('selected')));
+							});
+						}
+					}
+					return Backbone.Collection.prototype.add.apply(this, arguments);
+				}
+			},
+			fetch: function () {
+				var that = this,
+					promise = $.Deferred();
+				this.obj.fetch().then(function () {
+					that.populate();
+					promise.resolve(that);
+				});
+				return promise;
+			},
+			populate: function () {
+				var that = this,
+					obj = this.obj,
+					array_key = this.options.array_key,
+					arr = obj.get(array_key) || [];
+				this.reset(arr);
+				obj.on('change:' + array_key, function (obj, arr) {
+					console.log('updating list -->', arguments);
+					that.reset(arr);
+				});
+				//console.log('obj --> ', obj, array_key);
+				//console.log('list --> ', arr);
+			},
+			save: function () {
+				var that = this,
+					promise = $.Deferred(),
+					array_key = that.options.array_key;
+				console.log('trying to save list = ', that.models);
+				this.obj.save(array_key, that.models).then(function () {
+					promise.resolve();
+				}).fail(function () { promise.reject('Could not save'); });
+				return promise;
+			},
+		});
+
+		var now = function () {
+			return (new Date()).valueOf();
+		};
+		var pop = function (arr) {
+			if (!_.isArray(arr)) { return arr; }
+			return _.first(arr);
+		};
+
+		return {
+			Model: Model,
+			Collection: Collection,
+			now: now
+		};
+
+	});
--- apps/todos/html/js/todos.js
+++ apps/todos/html/js/todos.js
@@ -1,165 +1,28 @@
 /* global angular, $, console, _, Backbone */
 angular
 	.module('todos', ['ui','indx'])
-	.controller('todos', function($scope, client, utils) {
+	.controller('todos', function($scope, client, utils, collection) {
 		'use strict';
 
 		var box,
 			u = utils;
 
-		var editable_item = {
-				create: function () {
-					var that = this;
-					console.log('creating item');
-					box.get_obj(this.id).then(function (new_obj) {
-						console.log('new item');
-						new_obj.save(that.newAttributes).then(function () {
-							console.log('saved');
-							that.trigger('created', new_obj);
-						});
-					});
-					return this;
-				},
-				remove: function () { console.warn('remove: override this'); },
-				edit: function (is_new) {
-					console.log('edit');
-					this.newAttributes = _.clone(this.attributes);
-					this._is_new = is_new;
-					this._is_editing = true;
-					return this;
-				},
-				stage_and_save: function () {
-					var that = this;
-					console.log('stage and save');
-					if (this.is_new()) {
-						console.log('new, so create');
-						return this.create();
-					} else {
-						this.save(this.newAttributes).then(function () {
-							console.log('saved');
-							that.restore();
-							u.safe_apply($scope);
-						});
-					}
-				},
-				restore: function () {
-					this._is_editing = false;
-					this.trigger('restore');
-				},
-				is_editing: function () { return this._is_editing; },
-				is_new: function () { return this._is_new; }
-			},
-			EditableList = Backbone.Collection.extend({
-				item_prototype: editable_item,
-				initialize: function (models) {
-					var that = this;
-					this.on('add', function () {
-						console.log('add...');
-						that.save();
-					});
-					this.on('add remove reset', function () {
-						that.set_new_model(that._new_model);
-					});
-					console.log('this is should call first!');
-					this.reset(models);
-					this.set_new_model();
-				},
-				new_model: function (attributes) {
-					var that = this,
-						model = new Backbone.Model(attributes);
-					this.extend_model(model);
-					model
-						.edit(true)
-						.set({ timestamp: now() })
-						.on('created', function (model) {
-							console.log('going for the add');
-							that.add(model);
-							that.set_new_model();
-						})
-						.on('restore', function () {
-							that.set_new_model();
-						});
-					console.log('new model',this._new_model, model);
-					console.log('old', this.all_models.length);
-					this.set_new_model(model);
-					console.log('new', this.all_models.length);
-					//this.reset(this.models); // TODO: ANGULAR GRR's
-					return model;
-				},
-				set_new_model: function (model) {
-					this._new_model = model;
-					this.all_models = model ? this.models.concat([ model ]) : this.models;
-					console.log('update!"!!!', this.models.length, this.all_models.length)
-					this.trigger('update', this);
-				},
-				extend_model: function (model) {
-					_.extend(model, this.item_prototype);
-					if (this.item_prototype.initialize) {
-						this.item_prototype.initialize.apply(model);
-					}
-					if (this.item_prototype.defaults) {
-						_.defaults(model.attributes, this.item_prototype.defaults);
-					}
-				},
-				add: function (model, options) {
-					var that = this;
-					if (_.isArray(model)) {
-						_.each(model, function (model) { that.add(model, options); });
-					} else {
-						if (!_.isObject(model)) { console.error('There was a non-object stored??', model); return; }
-						this.extend_model(model);
-						return Backbone.Collection.prototype.add.apply(this, arguments);
-					}
-				},
-				save: function () { console.log('save: override this'); }
-			});
-
-		var todo_list_prototype = _.extend({}, editable_item, {
+		var TodoList = collection.Model.extend({
 				defaults: { title: 'Todo list' },
 				initialize: function () {
 					var that = this;
-					console.log('curr todos', this.get('todos'));
-					this.todos = new TodoListItems(this.get('todos'), { todo_list: this });
+
+					this.todos = new TodoListItems(undefined, {
+						box: box,
+						obj: this,
+						array_key: 'todos'
+					});
+
 					that.update_todo_list();
-					this.on('change:todos', function () {
-						console.log('woah, stuff is changing!');
-						that.todos.reset(that.get('todos'));
-					});
-					console.log('@DWS', this.todos.models.length, this.todos.all_models.length)
+
 					this.todos.on('update change', function () {
-						console.log('kk', that);
 						that.update_todo_list();
 					});
-					this.todos.on('add', function (todo) {
-						that.save_todo_list(todo);
-					});
-				},
-				save_todo_list: function () {
-					var that = this,
-						promise = $.Deferred();
-					console.log('save todo list');
-					that.save('todos', that.todos.models).then(function () {
-						that.update_todo_list();
-						promise.resolve();
-					});
-					return promise;
-				},
-				delete_todo: function (todo) {
-					var that = this,
-						promise = $.Deferred();
-					console.log('delete todo');
-					this.fetch().then(function () {
-						that.todos.remove(todo);
-						console.log('destroy obj');
-						todo.destroy(function () {
-							console.log('update list');
-							that.save('todos', that.todos.models).then(function () {
-								that.update_todo_list();
-								promise.resolve();
-							});
-						});
-					});
-					return promise;
 				},
 				update_todo_list: function () {
 					var that = this;
@@ -169,7 +32,6 @@ angular
 								var t = pop(todo.get('today'));
 								return today ? t === true : t !== true;
 							});
-						console.log('FILTERED TODOS', that.todos.all_models)
 						return {
 							title: group_title,
 							todos: filtered_todos,
@@ -178,100 +40,52 @@ angular
 					});
 					console.log('todos, ', that.todos_by_group);
 					u.safe_apply($scope);
-				},
-				show: function () {
-					console.log('show todo list ', this);
-					$scope.curr_todo_list = this;
-				},
-				new_todo: function () {
-					var that = this,
-						id = 'todo-item-' + now(),
-						todo = this.todos.new_model({ id: id });
-
-					todo.on('restore', function () {
-						if (todo.is_new()) { that.todos.remove(todo); }
-					});
-				},
-				is_shown: function () {
-					return $scope.curr_todo_list === this;
 				}
 			}),
 
-			TodoLists = EditableList.extend({
-				item_prototype: todo_list_prototype,
-				fetch: function () {
-					if (!box) { u.debug('no box, skipping '); return ;}
-					var that = this,
-						promise = $.Deferred();
-					box.get_obj('todo_app').then(function (todo_app) {
-						var todo_lists = todo_app.get('todo_lists') || [];
-						that.reset(todo_lists);
-						todo_app.on('change:todo_lists', function (todo_app, todo_lists) {
-							console.log('updating lists -->', arguments);
-							that.reset(todo_lists);
-						});
-						console.log('lists --> ', todo_lists);
-						promise.resolve(that);
-					});
-					return promise;
-				},
-				save: function () {
-					var that = this,
-						promise = $.Deferred();
-					console.log('save todo lists ...');
-					box.get_obj('todo_app').then(function (todo_app) {
-						console.log('trying to save todo lists = ', that.models);
-						todo_app.save('todo_lists', that.models).then(function () {
-							promise.resolve();
-						});
-					}).fail(function () { u.error('Could not create new todo list'); });
-					return promise;
+			TodoLists = collection.Collection.extend({
+				model: TodoList,
+				new_model: function () {
+					var id = 'todo-list-' + collection.now();
+					return collection.Collection.prototype.new_model.call(this,
+						{ id: id }, { select: true });
 				}
-			}),
-			todo_lists = new TodoLists();
+			});
 
-		var todo_list_item_prototype = _.extend({}, editable_item, {
+		var TodoListItem = collection.Model.extend({
 				defaults: { title: '', urgency: 'low', completed: false },
 				toggle: function () {
 					this.save('completed', !pop(this.get('completed')));
 				},
-				remove: function () {
+				/*remove: function () {
 					console.log('wrtf', this, this.collection)
 					this.collection.todo_list.delete_todo(this);
-				}
+				}*/
 			}),
-			TodoListItems = EditableList.extend({
-				item_prototype: todo_list_item_prototype,
-				initialize: function (models, options) {
-					this.todo_list = options.todo_list;
-					console.log('p', this.todo_list)
-					EditableList.prototype.initialize.apply(this, arguments);
+			TodoListItems = collection.Collection.extend({
+				model: TodoListItem,
+				new_model: function () {
+					var id = 'todo-item-' + collection.now();
+					return collection.Collection.prototype.new_model.call(this,
+						{ id: id });
 				}
 			});
 
 
 		var initialize = function () {
-			todo_lists.fetch().then(function () {
+			console.log('init');
+			$scope.todo_lists = new TodoLists(undefined, {
+				box: box,
+				obj: box._create_model_for_id('todo_app'),
+				array_key: 'todo_lists',
+				saveSelected: true
+			});
+			$scope.todo_lists.fetch();
+			$scope.todo_lists.on('update change', function () {
+				//console.log('fetched');
 				u.safe_apply($scope);
 			});
 		};
-
-		var create_todo_list = function () {
-			var id = 'todo-list-' + now(),
-				todo_list = todo_lists.new_model({ id: id });
-
-			todo_list.on('restore', function () {
-				if (todo_list.is_new()) { $scope.curr_todo_list = undefined; }
-			}).on('created', function (todo_list) {
-				$scope.curr_todo_list = todo_list;
-			});
-
-			$scope.curr_todo_list = todo_list;
-		};
-
-
-
-
 
 		var sorters = [
 			{	key: 'priority',
@@ -298,9 +112,6 @@ angular
 		var to_date_string = function (d) { return (new Date(d)).toDateString(); };
 		var to_time_string = function (d) { return (new Date(d)).toLocaleTimeString(); };
 
-		var now = function () {
-			return (new Date()).valueOf();
-		};
 
 		var pop = function (arr) {
 			if (!_.isArray(arr)) { return arr; }
@@ -328,9 +139,6 @@ angular
 		window.$scope = $scope;
 
 		_.extend($scope, {
-			todo_lists: todo_lists,
-			curr_todo_list: undefined,
-			create_todo_list: create_todo_list,
 			to_date_string: to_date_string,
 			to_time_string: to_time_string,
 			pop: pop,
