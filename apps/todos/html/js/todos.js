ae1b74942cf1ff9f12b24d965a19e51b9b03ab5b
Peter West <peter.g.west@gmail.com>
2013-09-18 14:41:12 +0100

    sorting and moving between lists

--- apps/todos/html/js/todos.js
+++ apps/todos/html/js/todos.js
@@ -185,116 +185,123 @@ angular
 					$('textarea').blur();
 					break;
 				}
+			} else {
+				switch (e.keyCode) {
+				case 27: //esc
+					$('[sortable]').sortable('cancel');
+					break;
+				}
 			}
 		});
 
 	}).directive('focusMe', function($timeout, $parse) {
 	  return {
-	    //scope: true,   // optionally create a child scope
-	    link: function(scope, element, attrs) {
-	      var model = $parse(attrs.focusMe);
-	      scope.$watch(model, function(value) {
-	        console.log('value=',value);
-	        if(value === true) {
-	          $timeout(function() {
-	            element[0].focus();
-	          });
-	        }
-	      });
-	    }
+		//scope: true,   // optionally create a child scope
+		link: function(scope, element, attrs) {
+		  var model = $parse(attrs.focusMe);
+		  scope.$watch(model, function(value) {
+			console.log('value=',value);
+			if(value === true) {
+			  $timeout(function() {
+				element[0].focus();
+			  });
+			}
+		  });
+		}
 	  };
 	}).directive('ngFocus', ['$parse', function($parse) {
-	  return function(scope, element, attr) {
-	    var fn = $parse(attr['ngFocus']);
-	    element.bind('focus', function(event) {
-	      scope.$apply(function() {
-	        fn(scope, {$event:event});
-	      });
-	    });
-	  }
-	}]).directive('draggable', function() {
-    	return {
-	        // A = attribute, E = Element, C = Class and M = HTML Comment
-	        restrict:'A',
-	        link: function(scope, element, attrs) {
-	        	element.draggable({
-			        revert:true,
-			        handle: "[draggable-handle]"
-			      });
-	        }
+		return function(scope, element, attr) {
+			var fn = $parse(attr['ngFocus']);
+			element.bind('focus', function (event) {
+				scope.$apply(function () {
+					fn(scope, { $event: event });
+				});
+			});
+		}
+	}]).directive('sortable', function (utils) {
+		return {
+			// A = attribute, E = Element, C = Class and M = HTML Comment
+			restrict: 'A',
+			link: function (scope, element, attrs) {
+				var lastLiAbove = undefined;
+				element.sortable({
+					revert: true,
+					handle: "[draggable-handle]",
+					placeholder: 'todo-placeholder',
+					tolerance: "pointer",
+					start: function (ev, ui) {
+						var height = ui.item.find('.todo-body').outerHeight() + 1,
+							tscope = angular.element(ui.item).scope();
+						ui.placeholder.height(height);
+						lastLiAbove = ui.placeholder.prev();
+						tscope.todo.is_dragging = true;
+					},
+					change: function (ev, ui) {
+						var liAbove = ui.placeholder.prev('li.todo-item'),
+							liBelow = ui.placeholder.next('li.todo-item');
+						if (lastLiAbove !== liAbove) {
+							var clone = ui.placeholder.clone(),
+								height = clone.height();
+							$('.todo-placeholder.clone').remove();
+							lastLiAbove.after(clone);
+							clone.addClass('animate clone').height(0)
+							ui.placeholder.removeClass('animate').height(0);
+							setTimeout(function () {
+								ui.placeholder.addClass('animate').height(height);
+							});
+							lastLiAbove = liAbove;
+							if (liBelow.length) {
+								var nextTodo = angular.element(liBelow).scope().todo,
+									tscope = angular.element(ui.item).scope(),
+									draggingTodo = tscope.todo;
+								draggingTodo.newAttributes.urgency = pop(nextTodo.get('urgency'));
+								utils.safe_apply(tscope);
+							}
+						}
+					},
+					stop: function (ev, ui) {
+						var tscope = angular.element(ui.item).scope(),
+							todo = tscope.todo,
+							todos = scope.todo_lists.selected.todos,
+							new_todos = element.find('li.todo-item').map(function () {
+								return angular.element(this).scope().todo;
+							}).get();
+						lastLiAbove = undefined;
+						todo.is_dragging = false;
+						todo.stage_and_save();
+						todos.reset(new_todos).save();
+						utils.safe_apply(tscope);
+						console.log(todos);
+					}
+				}).disableSelection();
+			}
 		};
-	}).directive('sortable', function(utils) {
-    	return {
-	        // A = attribute, E = Element, C = Class and M = HTML Comment
-	        restrict:'A',
-	        link: function(scope, element, attrs) {
-	        	var lastLiAbove = undefined;
-	        	element.sortable({
-			        revert: true,
-			        handle: "[draggable-handle]",
-			        placeholder: 'todo-placeholder',
-			        start: function (ev, ui) {
-			        	var height = ui.item.find('.todo-body').outerHeight() + 1,
-			        		tscope = angular.element(ui.item).scope();
-			        	ui.placeholder.height(height);
-			        	lastLiAbove = ui.placeholder.prev();
-			        	tscope.todo.is_dragging = true;
-			        },
-			        change: function (ev, ui) {
-			        	var liAbove = ui.placeholder.prev('li.todo-item'),
-			        		liBelow = ui.placeholder.next('li.todo-item');
-			        	if (lastLiAbove !== liAbove) {
-			        		var clone = ui.placeholder.clone(),
-			        			height = clone.height();
-			        		$('.todo-placeholder.clone').remove();
-			        		lastLiAbove.after(clone);
-			        		clone.addClass('animate clone').height(0)
-			        		ui.placeholder.removeClass('animate').height(0);
-			        		setTimeout(function () {
-			        			ui.placeholder.addClass('animate').height(height);
-			        		});
-			        		lastLiAbove = liAbove;
-			        		if (liBelow.length) {
-			        			var nextTodo = angular.element(liBelow).scope().todo,
-			        				tscope = angular.element(ui.item).scope(),
-			        				draggingTodo = tscope.todo;
-			        			draggingTodo.newAttributes.urgency = pop(nextTodo.get('urgency'));
-			        			utils.safe_apply(tscope);
-			        		}
-			        	}
-			        },
-			        stop: function (ev, ui) {
-			        	var tscope = angular.element(ui.item).scope(),
-			        		todo = tscope.todo,
-			        		todos = scope.todo_lists.selected.todos,
-			        		new_todos = element.find('li.todo-item').map(function () {
-			        			return angular.element(this).scope().todo;
-			        		}).get();
-			        	lastLiAbove = undefined;
-			        	todo.is_dragging = false;
-			        	todo.stage_and_save();
-			        	todos.reset(new_todos).save();
-			        	utils.safe_apply(tscope);
-			        	console.log(todos);
-			        }
-			      });
-	        }
-		};
-	}).directive('droppable-above', function() {
-    	return {
-	        // A = attribute, E = Element, C = Class and M = HTML Comment
-	        restrict:'A',
-	        link: function(scope, element, attrs) {
-	        	element.draggable({
-			        revert:true,
-			        handle: "[draggable-handle]"
-			      });
-	        }
+	}).directive('droppable', function () {
+		return {
+			// A = attribute, E = Element, C = Class and M = HTML Comment
+			restrict:'A',
+			link: function(scope, element, attrs) {
+				console.log('even working??')
+				element.droppable({
+					hoverClass: 'dropping',
+					tolerance: "pointer",
+					drop: function (ev, ui) {
+						console.log('drop', ui);
+						var el = ui.draggable,
+							draggable_scope = angular.element(el).scope(),
+							droppable_scope = scope,
+							old_todo_list = scope.todo_lists.selected.todos,
+							new_todo_list = scope.todo_list.todos,
+							todo = draggable_scope.todo;
+						old_todo_list.move(todo, new_todo_list);
+					}
+				});
+			}
 		};
 	});
 
 
-		var pop = function (arr) {
-			if (!_.isArray(arr)) { return arr; }
-			return _.first(arr);
-		};
+var pop = function (arr) {
+	if (!_.isArray(arr)) { return arr; }
+	return _.first(arr);
+};
