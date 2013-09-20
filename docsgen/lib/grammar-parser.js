398b642ad9f188145ac73704a2c68d1fe8938573
Peter West <peter.g.west@gmail.com>
2013-09-20 12:57:10 +0100

    added @include to grammars (not part of pegjs :'( )

--- docsgen/lib/grammar-parser.js
+++ docsgen/lib/grammar-parser.js
@@ -1,18 +1,50 @@
+/* jshint node:true */
 (function () {
 	'use strict';
 
 	var fs = require('fs'),
+		path = require('path'),
 		_ = require('underscore'),
 		Promise = require('node-promise').Promise,
+		allPromises = require('node-promise').all,
 		peg = require('pegjs');
 
 	var GrammarParser = function (grammarFile) {
 		var that = this;
 		this.loaded = new Promise();
+		readGrammar(grammarFile)
+			.then(function (grammar) {
+				that.parser = peg.buildParser(grammar);
+				that.loaded.resolve();
+			});
+	};
+
+	var readGrammar = function (grammarFile) {
+		var promise = new Promise();
 		fs.readFile(grammarFile, function (err, data) {
-			that.parser = peg.buildParser(data.toString());
-			that.loaded.resolve();
+			if (err) {
+				throw err;
+			}
+			var grammar = data.toString(),
+				promises = [];
+
+			grammar = grammar.replace(/^@import\s+"([^"]+)"/, function (match,
+				filename) {
+				var promise = new Promise();
+				promises.push(promise);
+				readGrammar(path.dirname(grammarFile) + '/' + filename)
+					.then(function (includedGrammer) {
+						grammar += includedGrammer;
+						promise.resolve();
+					});
+				return '';
+			});
+			allPromises(promises)
+				.then(function () {
+					promise.resolve(grammar);
+				});
 		});
+		return promise;
 	};
 
 	_.extend(GrammarParser.prototype, {
