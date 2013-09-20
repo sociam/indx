c86967a3a72d4bd72fb083b8a31ab6ed554516d7
Peter West <peter.g.west@gmail.com>
2013-09-20 12:01:07 +0100

    fixed add todo

--- apps/todos/html/js/todos.js
+++ apps/todos/html/js/todos.js
@@ -1,143 +1,14 @@
-/* global angular, $, console, _, confirm */
+/* global angular, $, console, _ */
 angular
 	.module('todos', ['ui', 'indx'])
-	.controller('todos', function ($scope, client, utils, collection) {
+	.controller('todos', function ($scope, client, utils, models) {
 		'use strict';
 
 		var box,
-			u = utils,
-			col = collection;
-
-		var TodoList = col.Model.extend({
-			defaults: {
-				title: 'Todo list'
-			},
-			initialize: function () {
-				var that = this;
-				this.todos = new TodoListItems(undefined, {
-					box: box,
-					obj: this,
-					array_key: 'todos'
-				});
-				this.todos
-					.on('update change', function () {
-						that.trigger('update');
-					})
-					.on('edit_change', function (todo) {
-						that.trigger('edit_change', todo);
-					});
-			},
-			remove: function () {
-				if (confirm('Are you sure you want to delete this todo list?')) {
-					return col.Model.prototype.remove.apply(this, arguments);
-				}
-				return this;
-			}
-		});
-
-		var TodoLists = col.Collection.extend({
-			model: TodoList,
-			new_model: function () {
-				var id = 'todo-list-' + col.now();
-				return col.Collection.prototype.new_model.call(this, {
-					id: id
-				}, {
-					select: true
-				});
-			}
-		});
-
-		var TodoListItem = col.Model.extend({
-			defaults: {
-				title: '',
-				urgency: 'low',
-				completed: false
-			},
-			toggle: function (new_state) {
-				var that = this,
-					old_state = this.get_attribute('completed');
-				new_state = _.isUndefined(new_state) ? !old_state : new_state;
-				if (new_state === old_state) {
-					return;
-				}
-				clearTimeout(this.removal_timeout);
-				if (new_state) {
-					this.just_completed = true;
-					this.undo = function () {
-						this.save('completed', old_state);
-						that.undo = undefined;
-						that.just_completed = false;
-						clearTimeout(this.removal_timeout);
-					};
-					this.removal_timeout = setTimeout(function () {
-						that.undo = undefined;
-						that.just_completed = false;
-						that.trigger('change');
-					}, 3300);
-				} else {
-					this.just_completed = false;
-				}
-				this.save('completed', new_state);
-			},
-			set_urgency: function (n) {
-				var urgency = this.get_staged_attribute(urgency),
-					i = urgencies.indexOf(urgency);
-				if (urgencies[i + n]) {
-					this.staged_attributes.urgency = urgencies[i + n];
-					//this.trigger('change');
-				}
-			},
-			restore: function () {
-				col.Model.prototype.restore.apply(this, arguments);
-				//this.trigger('change');
-			},
-			remove: function () {
-				if (confirm('Are you sure you want to delete this todo?')) {
-					return col.Model.prototype.remove.apply(this, arguments);
-				}
-				return this;
-			}
-		});
-
-		var TodoListItems = col.Collection.extend({
-			model: TodoListItem,
-			initialize: function () {
-				var that = this;
-				this.on('add reset', function (models) {
-					if (models.models) { // collection
-						models = models.models;
-					} else { //model
-						models = [models];
-					}
-					_.each(models, function (model) {
-						model
-							.on('edit', function () {
-								that.trigger('edit_change', model);
-							})
-							.on('restore', function () {
-								that.trigger('edit_change');
-							});
-					});
-				});
-				col.Collection.prototype.initialize.apply(this, arguments);
-			},
-			new_model: function () {
-				var id = 'todo-item-' + col.now();
-				return col.Collection.prototype.new_model.call(this, {
-					id: id
-				});
-			},
-			comparator: function (m) {
-				var urgency = m.is_editing ? m.get_staged_attribute('urgency') :
-						m.get_attribute('urgency'),
-					completed = m.get_attribute('completed') && !m.just_completed;
-				return completed ? 100 : -urgencies.indexOf(urgency);
-			}
-		});
-
+			u = utils;
 
 		var initialize = function () {
-			$scope.todo_lists = new TodoLists(undefined, {
+			$scope.todo_lists = new models.TodoLists(undefined, {
 				box: box,
 				obj: box._create_model_for_id('todo_app'),
 				array_key: 'todo_lists',
@@ -157,7 +28,6 @@ angular
 				.fetch();
 		};
 
-		var urgencies = ['low', 'med', 'high', 'urgent'];
 
 		// watches the login stts for changes
 		$scope.$watch('selected_box + selected_user', function () {
