7d3dfb0593ba5ff02f0fa184dbc554f35b98f011
Peter West <peter.g.west@gmail.com>
2013-10-02 16:43:48 +0100

    documentation generation on http

--- apps/devtools/__init__.py
+++ apps/devtools/__init__.py
@@ -15,13 +15,17 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, logging, json
+import os, logging, json, re
 from indx.webserver.handlers.base import BaseHandler
 from twisted.web.server import NOT_DONE_YET
 import requests
 from subprocess import check_output
 
+
+
 class DevToolsApp(BaseHandler):
+
+
     def __init__(self, server):
         BaseHandler.__init__(self, server)
         logging.debug(' hello')
@@ -42,35 +46,85 @@ class DevToolsApp(BaseHandler):
             out = check_output('karma start /home/peter/webbox/lib/indx/tests/docsgen.config.js', shell=True)
             self.return_ok(request, data = { "response": out })
 
-    def docs(self, request):
-        """ Get a list of tests.
-        """
-        logging.debug('getting tests')
+    def config_data(self, filename):
+        name = filename.split('/')[-1].split('.')[0];
+        return {
+            'name': name,
+            'config_filename': filename,
+            'docs_path': 'apps/devtools/docs/%s' % name,
+            'docs_url': 'apps/devtools/docs/%s' % name
+            }
+
+    def lookup_config(self, name):
+        docs = self.get_docs_list()
+        matches = [doc for doc in docs if doc['name'] == name]
+        if not matches:
+            self.return_forbidden(request) # todo: more useful return
+        return matches[0];
+
+
+    def get_docs_list(self):
+        logging.debug('getting list of doc configs')
         currdir = os.path.dirname(os.path.abspath(__file__))
-        tdir = os.path.sep.join([currdir, '..', '..', 'lib', 'indx', 'docs'])
-        files = [o for o in os.listdir(tdir)]
-        logging.debug("docs configs: {0}".format(files))
+        tdir = os.path.sep.join([currdir, '..', '..', 'docsgen', 'config'])
+        # todo find in doc configs apps
+        docs = [self.config_data(tdir + '/' + f) for f in os.listdir(tdir)]
+        logging.debug("doc configs: {0}".format(docs))
+        return docs;
 
-        self.return_ok(request, data = {"response": files})
+    def list_docs(self, request):
+        """ Get a list of doc configs.
+        """
+        docs = self.get_docs_list()
+        self.return_ok(request, data = { "response": docs })
 
+    def generate_doc(self, request):
+        """ Generate documentation from config file.
+        """
+        logging.debug('trying to generate docs')
+        #args = self.get_post_args(request)
+        #if not hasattr(args, 'name'):
+        #    self.return_forbidden(request)
+        #    return
+        #config_name = args['name'][0]
+        config_name = 'indx-collection'
+        config = self.lookup_config(config_name)
+        logging.debug('checking name %s' % config['name'])
+        logging.debug('generating doc %s' % config['name'])
+        logging.debug('node docsgen/build.js %s ' % config['config_filename']);
+        out = check_output('node docsgen/build.js %s --output-directory=%s' % (config['config_filename'], config['docs_path']), shell=True)
+        logging.debug(out);
+        self.return_ok(request, data = { "response": out })
+
+
+DevToolsApp.base_path = "devtools/api"
 
 DevToolsApp.subhandlers = [
     {
-        "prefix": "devtools/api/tests",
+        "prefix": "tests",
         'methods': ['GET', 'PUT'],
         'require_auth': False,
         'require_token': False,
         'handler': DevToolsApp.tests,
-        'accept':['*'],
+        'accept':['application/json'],
         'content-type':'application/json'
         },
     {
-        "prefix": "devtools/api/docs",
+        "prefix": "devtools/api/docs/list_docs",
         'methods': ['GET'],
         'require_auth': False,
         'require_token': False,
-        'handler': DevToolsApp.docs,
-        'accept':['*/*'],
+        'handler': DevToolsApp.list_docs,
+        'accept':['application/json'],
+        'content-type':'application/json'
+        },
+    {
+        "prefix": "devtools/api/docs/generate_doc",
+        'methods': ['POST'],
+        'require_auth': False,
+        'require_token': False,
+        'handler': DevToolsApp.generate_doc,
+        'accept':['application/json'],
         'content-type':'application/json'
         }
 ]
