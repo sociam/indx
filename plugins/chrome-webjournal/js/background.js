77ab42262cd35a2487b6d2fe0cd86d9dcaea4637
electronic Max <max@hip.cat>
2013-11-03 00:24:34 +0000

    plugin seems to work

--- plugins/chrome-webjournal/js/background.js
+++ plugins/chrome-webjournal/js/background.js
@@ -1,62 +1,71 @@
 
 (function() {
-
-    var set_up_store = function(client,utils) {
+    var server;
+    var connect = function(client,utils) {
         var server_url = localStorage.indx_url || 'https://indx.local:8211';
-        var s = new client.Store({server_host:server_url});
+        if (server === undefined || server.get('server_host') !== server_url) {
+            server = new client.Store({server_host:server_url});
+        }
         var d = utils.deferred();
-        try {
-            s.check_login().then(function(response) { 
-                if (response.is_authenticated) { 
-                    return d.resolve(s); 
-                };
-                d.reject('not logged in');
-            }).fail(d.reject);
-        } catch (e)  { console.error(e); }
+        server.check_login().then(function(response) {
+            console.log('check login response >>>>>> ', response);
+            if (response.is_authenticated) {  
+                return d.resolve(server, response);  
+            };
+            d.reject('not logged in');
+        }).fail(d.reject);
         return d.promise();
     };
     angular.module('webjournal', ['indx'])
-    .controller('options', function($scope, watcher, client,utils) {
-        setInterval(function()  {
-               s.check_login().then(function(result) {
-                console.log('result', result);
+    .controller('options', function($scope, watcher, client, utils) {
+        // options screen only  -------------------------------------------
+        var helper = function() {
+            var me = arguments.callee;
+            connect(client,utils).then(function(server, result) {
                 utils.safe_apply($scope, function() {
                     if (!result.is_authenticated) {
-                        return $scope.user_logged_in = 'not logged in';
+                        $scope.user_logged_in = 'not logged in';
+                        return setTimeout(function() { console.info('not logged in, trying again '); me(); }, 1000);
                     }
                     $scope.user_logged_in = result.name || result.username;
+                    server.on('disconnect', function() { 
+                        console.info('received ws:disconnect, waiting 10 and reconnecting ');
+                        setTimeout(me, 10000); 
+                    });
                 });
-            }).fail(function() { 
-                   utils.safe_apply($scope, function() { 
-                       $scope.user_logged_in = 'error connecting';
-                   });
+            }).fail(function(err) { 
+                utils.safe_apply($scope, function() { $scope.user_logged_in = 'error connecting'; });
+                setTimeout(function() { console.log('error connecting ', err, 'going again >>'); me(); }, 1000);
             });
-        }, 1000);
+        };
+        helper();
     }).controller('main', function($scope, watcher, client,utils) {
+        // main 
         window.utils = utils;
         var init = function(store) { 
-            console.log('success ', store);
-            var box = localStorage.indx_box || 'lifelog';
+            window.s = store;
+            console.log('connect success >>', store);
             var winstance = watcher.init(store);
-            console.log('setting box ', box);
-            try { 
-                winstance.set_box(localStorage.indx_box || 'lifelog');
-            } catch(e) { console.error(e); }
-            console.info("indx webjournal starting up");
+            store.on('disconnect', function() { 
+                winstance.set_box(); // disconnect box
+                console.error('diconnect >> waiting 1 second before reconnection');
+                setTimeout(function() {  runner(); }, 1000); 
+            });
         };
-        var s = set_up_store(client,utils).then(init).fail(function(err) {
-             console.error(' fail on set up store - do it  again')
-             // var me = arguments.callee;
-             // console.error(err);
-             // setTimeout(function() { set_up_store(client,utils).then(init).fail(me); }, 1000);
-         });
+        var runner = function() { 
+            var me = arguments.callee;
+            connect(client,utils)
+                .then(init)
+                .fail(function(err) { console.error('cannot connect -- ', err); setTimeout(me, 10000); });
+        };
+        runner();
     }).factory('watcher', function(utils, client) {
         var WindowWatcher = Backbone.Model.extend({
             initialize:function(attributes) {
                 console.log('initialise .. ');
                 var this_ = this;
                 this.data = [];
-                this.bind("change", function() { this_.change.apply(this_, arguments); });
+                this.bind("change", function() {  this_.change.apply(this_, arguments);   });
                 // created
                 chrome.windows.onCreated.addListener(function(window) {
                     chrome.tabs.getSelected(window, function(tab) {
@@ -91,8 +100,6 @@
                         this_.trigger("change", tab !== undefined ? tab.url : undefined);
                     });
                 });
-               console.log(' end initialise .. ');
-
             },
             start_polling:function(interval) {
                 var this_ = this;
@@ -109,7 +116,16 @@
                     delete this.poll;
                 }
             },
+            load_appropriate_box:function(client) {
+                console.log('load appropriate box >>>>>>>>>> ', localStorage.indx_box || 'lifelog'); 
+                this.set_box(localStorage.indx_box || 'lifelog');
+            },
             set_box:function(bid) {
+                if (!bid) { 
+                    // resetting...
+                    delete this.box;
+                    return;                    
+                }
                 console.log('attempting to set box >> ', bid);
                 var this_=  this, store = this.get('store');
                 store.get_box(bid).then(function(b) {  
@@ -117,36 +133,39 @@
                     console.info('successfully got box ', bid);
                     this_.box = b;  
                 }).fail(function(err) {
-                    u.error('error getting box ', bid);
+                    console.error('!!!!!! error getting box ', bid);
                     delete this_.box;
                 });
             },
             change:function(url) {
                 var this_ = this;
+                var now = new Date();
                 if (this.current_record !== undefined) {
-                    if (url == this.current_record.location) {
-                        this.current_record.end = new Date();
-                        this._record_updated(this.current_record);
-                        this.trigger("update_record", this.current_record);
+                    this.current_record.end = now;
+                    this._record_updated(this.current_record);
+                    if (url === this.current_record.location) { 
+                        // we're done
                         return;
+                    } else {
+                        // different now
+                        console.log('new record!');
+                        delete this.current_record;
+                        this.trigger("new_record", this.current_record);
                     }
-                    // different now
-                    delete this.current_record;
-                    this.trigger("new_record", this.current_record);
                 }
                 // go on to create a new record
                 if (url !== undefined) {
-                    var now = new Date();
                     this.current_record = this.make_record({start: now, end:now, to: url, location: url});
                     this.data.push(this.current_record);
                     this._record_updated(this.current_record);
                 }
             },
             make_record:function(options) {
-                console.log("make record >> ", options);
+                // console.log("make record >> ", options);
                 return _({}).chain().extend(options).extend({id:utils.guid()}).value();
             },
             _record_updated:function() {
+                console.log('record updated ... box ', this.box, this.current_record);
                 var this_ = this;
                 if (this.box !== undefined && this.current_record !== undefined) {
                     var id = "webjournal-log-"+this.current_record.id, box = this_.box;
@@ -160,8 +179,17 @@
         });
     return {
         init:function(store) {
-            this.watcher = new WindowWatcher({store:store});
+            if (!this.watcher) { 
+                this.watcher = new WindowWatcher({store:store});
+            }
+            this.watcher.load_appropriate_box();
             return this.watcher;
+        },
+        set_store:function(store) { 
+            if (this.watcher) { 
+                this.watcher.set('store', store); 
+                this.watcher.load_appropriate_box();
+            }
         },
         set_polling:function(polling) {
             polling ? this.watcher.start_polling() : this.watcher.end_polling();
