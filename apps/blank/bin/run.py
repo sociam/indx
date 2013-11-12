5c3713ecab372914a82458c05da3253da93ae5c0
aprilush <aprilush@gmail.com>
2013-11-12 20:12:48 +0000

    trying the keyring lib in run.py

--- apps/blank/bin/run.py
+++ apps/blank/bin/run.py
@@ -1,19 +1,25 @@
 import logging,json,argparse,sys
 import logging.config
+import keyring
+import keyring.util.platform_
 
 if __name__ == '__main__':
-	# parse out the parameters
-	parser = argparse.ArgumentParser(prog="run")
-	parser.add_argument('--config')
-	parsed = parser.parse_args()
-	print parsed
+    # parse out the parameters
+    parser = argparse.ArgumentParser(prog="run")
+    parser.add_argument('--config')
+    parsed = parser.parse_args()
+    print parsed
 
 logging.basicConfig(filename="blank.log", level=logging.DEBUG)
 args = vars(parser.parse_args())
 if args['config']:
     config = json.loads(args['config'])
     logging.debug("received config: {0}".format(config))
+    keyring.set_password("INDX", "INDX_Blank_App", config)
 else:
-	logging.debug("running the app");
-	# tadaaa
+    print(keyring.util.platform_.data_root())
+    config = keyring.get_password("INDX", "INDX_Blank_App")
+    logging.debug("running the app with: {0}".format(config));
 
+# keyring.set_password("INDX", "INDX_Blank_App", "{'password':'asdf', 'user':'laura', 'box':'blankie'}")
+# print keyring.get_password("INDX", "INDX_Blank_App")
