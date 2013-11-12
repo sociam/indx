c40f5ac9c9d2a6f68f22711423e2ca5956488168
electronic Max <max@hip.cat>
2013-11-12 17:17:08 +0000

    run

--- apps/blank/__init__.py
+++ apps/blank/__init__.py
@@ -1,8 +1,8 @@
+
 import logging, json, subprocess
+
 from indx.webserver.handlers.base import BaseHandler
-import requests
 from indxclient import IndxClient
-from blank import Blank
 
 class BlankApp(BaseHandler):
 
@@ -14,7 +14,7 @@ class BlankApp(BaseHandler):
         manifest_data.close()
 
     def _get_indx_creds(self):
-        
+        pass
         
     def set_config(self, request): # what params?
         # when we want to change something in the config
@@ -37,16 +37,20 @@ class BlankApp(BaseHandler):
     def start(self):
         if check_auth():
             # do the stuff
-
+            pass
         else:
             # error because the app is not authorised
+            pass
+        pass
 
     def stop(self):
         if check_auth():
             # stop me!
+            pass
         else:
             # can't stop me!
-
+            pass
+        pass
 
 
 APP = BlankApp
