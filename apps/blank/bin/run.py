bdb3843cd995f355f6db567137163bf12f71f83a
aprilush <aprilush@gmail.com>
2013-11-12 18:50:58 +0000

    config setting and logging in run.py

--- apps/blank/bin/run.py
+++ apps/blank/bin/run.py
@@ -1,4 +1,5 @@
 import logging,json,argparse,sys
+import logging.config
 
 if __name__ == '__main__':
 	# parse out the parameters
@@ -7,10 +8,11 @@ if __name__ == '__main__':
 	parsed = parser.parse_args()
 	print parsed
 
+logging.basicConfig(filename="blank.log", level=logging.DEBUG)
 args = vars(parser.parse_args())
 if args['config']:
-    logging.debug("received config: {0}".format(args['config']))
     config = json.loads(args['config'])
+    logging.debug("received config: {0}".format(config))
 else:
 	logging.debug("running the app");
 	# tadaaa
