30c043b1841520a68f790159dd0b78d31267a226
electronic Max <max@hip.cat>
2013-11-12 17:45:29 +0000

    more progress

--- apps/blank/html/js/blank.js
+++ apps/blank/html/js/blank.js
@@ -1,5 +1,23 @@
 angular
 	.module('BlankApp', ['ui','indx'])
 	.controller('ConfigPage', function($scope, client, utils) {
+		var s = client.store;
 		window.store = client.store;
+
+		var load_box = function(bid) { 
+			s.getBox(bid).then(function(box) {
+				console.log('box ', box);
+				window.box = box;
+			});
+		};
+
+		$scope.setConfig = function(config) { 
+			console.info('i got a config ', config);
+		};
+
+		$scope.$watch('selectedUser + selectedBox', function() { 
+			if ($scope.selectedBox) {
+				load_box($scope.selectedBox);	
+			}
+		});
 	});
