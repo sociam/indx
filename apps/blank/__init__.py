85a186e3b372710e10ba47bc3d3de5a52a4548f1
aprilush <aprilush@gmail.com>
2013-11-12 17:14:59 +0000

    test app that doesn't do anything

--- apps/blank/__init__.py
+++ apps/blank/__init__.py
@@ -1,36 +1,52 @@
-import logging, json
+import logging, json, subprocess
 from indx.webserver.handlers.base import BaseHandler
 import requests
 from indxclient import IndxClient
-from blank import BlankApp
+from blank import Blank
 
 class BlankApp(BaseHandler):
 
     def __init__(self, server):
         BaseHandler.__init__(self, server)
         self.isLeaf = True
-        self.app = BlankApp() # with any params a real app would need
+        manifest_data = open('manifest.json')
+        self.manifest = json.load(manifest_data)
+        manifest_data.close()
 
-    def config(self): # what params?
-    	if "first use":
-    		# how do we check that it's first use?
-    	else :
-    		# when we want to change somthing in the config
+    def _get_indx_creds(self):
+        
+        
+    def set_config(self, request): # what params?
+        # when we want to change something in the config
+        # invoke external process to set their configs
+        
+        # get theconfiguraiton we want to set out of request
+        # do that thang here
+        config = {}
+        jsonconfig = json.dumps(config)
+        # somewhere inside this we have put {0} wildcard so we wanna substitute that
+        # with the actual config obj
+        expanded = [x.format(jsonconfig) for x in self.manfiest['config']]
+        subprocess.call(expanded)
 
-    def check_auth(self):
-    	# check if the app can do stuff 
-    	return False
+
+    def _check_auth(self):
+        # check if the app can do stuff 
+        return False
 
     def start(self):
-    	if check_auth():
-    		# do the stuff
-    	else:
-    		# error because the app is not authorised
+        if check_auth():
+            # do the stuff
+
+        else:
+            # error because the app is not authorised
 
     def stop(self):
-    	if check_auth():
-    		# stop me!
-    	else:
-    		# can't stop me!
+        if check_auth():
+            # stop me!
+        else:
+            # can't stop me!
+
+
 
 APP = BlankApp
