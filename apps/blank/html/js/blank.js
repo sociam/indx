dd32d474a6b2aa408fa0d2c6455f45d7bbb8f2bb
electronic Max <max@hip.cat>
2013-11-12 18:52:12 +0000

    small changes to html

--- apps/blank/html/js/blank.js
+++ apps/blank/html/js/blank.js
@@ -8,6 +8,22 @@ angular
 			s.getBox(bid).then(function(box) {
 				console.log('box ', box);
 				window.box = box;
+
+				// get the users
+				s.getUserList().then(function(u) { 
+					sa(function() { $scope.users = u; });
+				}).fail(function(e) {
+					sa(function() { $scope.status = 'error getting user list'; });
+					console.error(e);
+				});
+
+				// get the users
+				s.getBoxList().then(function(boxes) { 
+					sa(function() { $scope.boxes = boxes; });
+				}).fail(function(e) {
+					sa(function() { $scope.status = 'error getting box list'; });
+					console.error(e);
+				});
 			});
 		};
 		$scope.setConfig = function(config) { 
