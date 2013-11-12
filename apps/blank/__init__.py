3fa87e57357ce5dd1402e73d78c7db837014fd44
electronic Max <max@hip.cat>
2013-11-12 17:53:53 +0000

    more progress on server

--- apps/blank/__init__.py
+++ apps/blank/__init__.py
@@ -22,17 +22,19 @@ class BlankApp(BaseHandler):
     def set_config(self, request): # what params?
         # when we want to change something in the config
         # invoke external process to set their configs
-        
+        logging.debug("set_config -- getting config from request")        
+        config = json.loads(self.get_arg(request, "config"))
+        logging.debug("blank config {0}".format(config))
         # get theconfiguraiton we want to set out of request
         # do that thang here
         manifest = self._load_manifest()
-        config = {}
         jsonconfig = json.dumps(config)
         # somewhere inside this we have put {0} wildcard so we wanna substitute that
         # with the actual config obj
         expanded = [x.format(jsonconfig) for x in self.manfiest['config']]
-        subprocess.call(expanded)
-
+        result = subprocess.call(expanded)
+        logging.debug("result of subprocess call {0}".format(result))
+        return self.return_ok(json.dumps(result))
 
     def _check_auth(self):
         # check if the app can do stuff 
@@ -55,6 +57,18 @@ class BlankApp(BaseHandler):
             # can't stop me!
             pass
         pass
+
+BlankApp.subhandlers = [
+    {
+        "prefix": "blank/api/set_config",
+        'methods': ['GET'],
+        'require_auth': True,
+        'require_token': True,
+        'handler': BlankApp.set_config,
+        'accept':['application/json'],
+        'content-type':'application/json'
+    }   
+]
 
 
 APP = BlankApp
