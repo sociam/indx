bf04bbf4fbc1ad951b2327a0892a9855d2f4c374
electronic Max <electronic@gmail.com>
2013-11-26 14:34:27 +0000

    oops forgot to commit

---
+++ html/js/launcher-box.js
@@ -0,0 +1,20 @@
+/*jslint vars:true, todo:true, sloppy:true */
+angular.module('launcher')
+	.directive('indxBoxlistBox',function() {
+		return {
+			restrict:'E',
+			replace:false,
+			templateUrl:'templates/box.html',
+			link:function($scope, $element) {
+				$scope.el = $element;
+			},
+			scope: { box:"=box" },
+			controller: function($scope, client, backbone, utils) {
+				var u = utils, sa = function(f) { utils.safe_apply($scope,f); };
+				$scope.obj_count = $scope.box ? $scope.box.getObjIDs().length : 0;
+				$scope.box.on('obj-add', function(result) { 
+					console.log('obj add!!!', result);
+				});
+			}
+		};
+	});
