fa5ee2fcc3dbfa3918b975626be36db73b517fca
aprilush <aprilush@gmail.com>
2013-12-04 18:04:06 +0000

    nikeplus harvester as a service, not doing anything other than setting / getting config atm

---
+++ apps/nikeplus/bin/nike_harvester.py
@@ -0,0 +1,176 @@
+import logging, json, argparse, sys, os, uuid, urllib2
+import logging.config
+import keyring, keyring.util.platform_
+from keyring.backends.pyfs import PlaintextKeyring
+from indxclient import IndxClient, IndxClientAuth
+from twisted.internet.defer import Deferred
+import oauth2 as oauth
+from time import sleep
+from datetime import date, datetime, timedelta, time
+from nikeplus import NikePlus
+
+class NikeHarvester:
+
+    def __init__(self):
+        logging.basicConfig(filename="nike_harvester.log", level=logging.DEBUG)
+    
+        data_root = keyring.util.platform_.data_root()
+        if not os.path.exists(data_root):
+            os.mkdir(data_root)
+        keyring.set_keyring(PlaintextKeyring())
+
+        self.parser = argparse.ArgumentParser(prog="run")
+        self.parser.add_argument('--config', help="Set config (input requires JSON) and exit.")
+        self.parser.add_argument('--get-config', action="store_true", help="Output current config as JSON and exit.")
+        self.parser.add_argument('--server', help="The server URL to connect to.")
+
+        self.nike = NikePlus()
+
+        self.version = 0
+
+        self.harvester_id = "nikeplus_harvester"
+        self.steps_ts_id = "nikeplus_steps_ts"
+        self.calories_ts_id = "nikeplus_calories_ts"
+        self.distance_ts_id = "nikeplus_distance_ts"
+        self.fuel_ts_id = "nikeplus_fuel_ts"
+
+    def set_config(self, args):
+        stored_config_harvester = keyring.get_password("INDX", "INDX_Nike_Harvester")
+        if stored_config_harvester is not None:
+            stored_config_harvester = json.loads(stored_config_harvester)
+        stored_config_nike = keyring.get_password("Nike.com", "Nike+")
+        if stored_config_nike is not None:
+            stored_config_nike = json.loads(stored_config_nike)
+
+        received_config = json.loads(args['config'])
+        if (type(received_config) != dict):
+            received_config = json.loads(received_config)
+        logging.debug("Received config: {0}".format(received_config))
+        if 'nike' in received_config:
+            nike_config = received_config['nike']
+            if nike_config and ('user' in nike_config) and ('password' in nike_config): 
+                logging.debug("Received user: {0} and password: {1}".format(nike_config['user'], nike_config['password']))
+                try:
+                    self.nike.login(nike_config['user'], nike_config['password'])
+                    logging.debug("Logged in with username {0} and password {1}", nike_config['user'], nike_config['password'])
+                    token = self.nike.get_token()
+                    logging.debug("Got token {0}".format(token))
+                    if token:
+                        keyring.set_password("Nike.com", "Nike+", json.dumps(nike_config))
+                except Exception as exc:
+                    logging.error("Could not authorise to Nike, with username {0} and password {1},  error: {2}".format(nike_config['user'], nike_config['password'], exc))
+        if 'harvester' in received_config:
+            harvester_config = received_config['harvester']
+            if harvester_config != stored_config_harvester:
+                keyring.set_password("INDX", "INDX_Nike_Harvester", json.dumps(harvester_config)) 
+
+    def get_config(self, args):
+        stored_config_harvester = keyring.get_password("INDX", "INDX_Nike_Harvester")
+        stored_config_nike = keyring.get_password("Nike.com", "Nike+")
+
+        logging.debug("Loaded harvester config from keyring: {0}".format(stored_config_harvester))
+        logging.debug("Loaded nike config from keyring: {0}".format(stored_config_nike))
+
+        if stored_config_nike is not None :
+            if stored_config_harvester is None :
+                return json.dumps({"nike":json.loads(stored_config_nike)}) 
+            return json.dumps({"nike":json.loads(stored_config_nike), "harvester":json.loads(stored_config_harvester)}) 
+        if stored_config_harvester is not None :
+            return json.dumps({"harvester":json.loads(stored_config_harvester)}) 
+        return json.dumps({})
+
+    def check_configuration(self):
+        stored_config_harvester = keyring.get_password("INDX", "INDX_Nike_Harvester")
+        logging.debug("stored_config_harvester type: {0}".format(type(stored_config_harvester)))
+        logging.debug("Loaded harvester config from keyring: {0}".format(stored_config_harvester))
+        if stored_config_harvester is None :
+            logging.error("Harvester not configured. Please configure before use.")
+            sys.exit(1)
+        if (type(stored_config_harvester) != dict):
+            stored_config_harvester = json.loads(stored_config_harvester)
+            logging.debug("stored_config_harvester type (after 1 loads): {0}".format(type(stored_config_harvester)))
+
+        token = self.nike.get_token()
+        if token is None:
+            stored_config_nike = keyring.get_password("Nike.com", "Nike+")
+            logging.debug("stored_config_nike type: {0}".format(type(stored_config_nike)))
+            logging.debug("Loaded nike config from keyring: {0}".format(stored_config_nike))
+
+            if stored_config_nike is None :
+                logging.error("No credentials for Nike.com. Please configure before use.")
+                sys.exit(1)
+            else :
+                if (type(stored_config_nike) != dict):
+                    stored_config_nike = json.loads(stored_config_nike)
+                    logging.debug("stored_config_nike type (after 1 loads): {0}".format(type(stored_config_nike)))
+                if ('password' in stored_config_nike) and ('user' in stored_config_nike):
+                    try:
+                        self.nike.login(stored_config_nike['user'], stored_config_nike['password'])
+                        logging.debug("Logged in with username {0} and password {1}", stored_config_nike['user'], stored_config_nike['password'])
+                        token = self.nike.get_token()
+                        logging.debug("Got token {0}".format(token))
+                    except Exception as exc: 
+                        logging.error("Could not authorise to nike, error: {1}".format(exc))
+                        sys.exit(1)
+
+        return stored_config_harvester['box'], stored_config_harvester['user'], stored_config_harvester['password'], stored_config_harvester['overwrite']
+
+    def run(self):
+        args = vars(self.parser.parse_args())
+        logging.debug("Received arguments: {0}".format(args))
+        if args['config']:
+            self.set_config(args)
+        elif args['get_config']:
+            print self.get_config(args)
+        else:
+            logging.debug("Starting the harvester. ")
+            self.work(args['server'])
+
+    def yesterday(self):
+        return datetime.combine((datetime.now()+timedelta(days=-1)).date(), time(00,00,00))
+
+    def today(self):
+        return datetime.combine(datetime.now().date(), time(00,00,00))
+
+    def get_indx(self, server_url, box, user, password):
+        return_d = Deferred()
+        def authed_cb(): 
+            def token_cb(token):
+                indx = IndxClient(server_url, box, appid, token = token)
+                return_d.callback(indx)
+
+            authclient.get_token(box).addCallbacks(token_cb, return_d.errback)
+            
+        authclient = IndxClientAuth(server_url, "INDX_Nike_Harvester")
+        authclient.auth_plain(user, password).addCallbacks(lambda response: authed_cb(), return_d.errback)
+        return return_d
+
+    def work(self, server_url):
+        box, user, password, overwrite = self.check_configuration()
+
+        def indx_cb(indx):
+            logging.debug("Created INDXClient.")
+
+            # harvester = self.find_create(indx, self.harvester_id, {"http://www.w3.org/2000/01/rdf-schema#label":"INDX Nike Harvester extra info"})
+            # if harvester :
+            #     if "zeros_from" in harvester :
+            #         all_zeros_from = datetime.strptime(harvester["zeros_from"][0]["@value"], "%Y-%m-%dT%H:%M:%S")
+            #         if day < all_zeros_from :
+            #             if overwrite :
+            #                 harvester["zeros_from"] = day.isoformat()
+            #     else :
+            #         harvester["zeros_from"] = day.isoformat()
+            # self.safe_update(indx, harvester)
+
+            # self.harvest(indx)
+
+        while 1: 
+            self.get_indx(server_url, box, user, password).addCallbacks(indx_cb, lambda failure: logging.error("Error: {0}".format(failure)))
+            logging.debug("Harvested! Suspending execution for 1 hour at {0}.".format(datetime.now().isoformat()))
+            sleep(3600)
+            overwrite = false; # will only overwrite the first time if the flag is set
+
+
+if __name__ == '__main__':
+    harvester = NikeHarvester()
+    harvester.run();
---
+++ apps/nikeplus/html/js/nike.js
@@ -0,0 +1,356 @@
+angular
+    .module('NikeHarvester', ['ui','indx'])
+    .controller('ConfigPage', function($scope, client, utils) {
+        var u = utils, s = client.store, sa = function(f) { utils.safeApply($scope, f); };
+        window.$s = $scope;
+        var config;
+
+        if ( !Date.prototype.toISOString ) {
+            ( function() {
+    
+            function pad(number) {
+                var r = String(number);
+                if ( r.length === 1 ) {
+                    r = '0' + r;
+                }
+                return r;
+            }
+         
+            Date.prototype.toISOString = function() {
+                return this.getUTCFullYear()
+                    + '-' + pad( this.getUTCMonth() + 1 )
+                    + '-' + pad( this.getUTCDate() )
+                    + 'T' + pad( this.getUTCHours() )
+                    + ':' + pad( this.getUTCMinutes() )
+                    + ':' + pad( this.getUTCSeconds() )
+                    + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
+                    + 'Z';
+            };
+            }() );
+        }
+
+        var status = function(stat) {
+            sa(function() { $scope.status = stat; });
+        };
+
+        var getConfigFromService = function() {
+            s._ajax('GET', 'apps/nikeplus/api/get_config').then(function(x) { 
+                console.log(x.config);
+                // var config = x.config;
+                var config = JSON.parse(x.config);
+                if (config.fitbit) {
+                    if (config.fitbit.token) {
+                        sa(function() { 
+                            _($scope).extend({ 
+                                token: config.fitbit.token
+                            });
+                        });
+                    } else if (config.fitbit.url) {
+                        sa(function() { 
+                            _($scope).extend({ 
+                                url: config.fitbit.url,
+                                req_token: config.fitbit.req_token
+                            });
+                        });
+                    }
+                }
+                if (config.harvester) {
+                    sa(function() { 
+                        _($scope).extend({ 
+                            password: config.harvester.password,
+                            box: config.harvester.box, 
+                            start: config.harvester.start, 
+                            overwrite: config.harvester.overwrite
+                        });
+                    });
+                    if (config.harvester.user && $scope.users) { 
+                        var match = $scope.users.filter(function(u) { return u['@id'] === config.harvester.user; });
+                        if (match.length) {
+                            console.log('match ', match[0]);
+                            window.match = match[0];
+                            sa(function() { $scope.user = match[0]; });
+                        }
+                    }
+                }
+                console.log($scope);
+            }).fail(function(err) { 
+                console.error('could not get config ', err);    
+            });
+        };
+
+        var getUsersAndBoxes = function() { 
+            var dul = u.deferred(), dbl = u.deferred();
+            // get the users
+            s.getUserList().then(function(users) {
+                console.log('users >> ', users);
+                window.users = users;
+                sa(function() { $scope.users = users.filter(function(f) { return f.type.indexOf('local') >= 0; });  });
+                dul.resolve();
+            }).fail(function(e) {
+                sa(function() { $scope.status = 'error getting user list'; });
+                console.error(e);
+                dul.reject();
+            });
+
+                // get the boxes
+            s.getBoxList().then(function(boxes) { 
+                sa(function() { $scope.boxes = boxes; });
+                dbl.resolve();
+            }).fail(function(e) {
+                sa(function() { $scope.status = 'error getting box list'; });
+                console.error(e);
+                dbl.reject();
+            });
+            return u.when([dul, dbl]);
+        };
+        $scope.grantACL = function(user,box) {
+            console.log('grantacl -- ', user, box);
+            s.getBox(box).then(function(b) { 
+                console.log('got box ', b.id);
+                b.setACL(user["@id"],{read:true,write:true}).then(function() {
+                    sa(function() { $scope.granted = true; $scope.granted_status = 'Success granting ' + user.name + " access to " + box; });
+                }).fail(function(e) {
+                    sa(function() { $scope.granted = true; $scope.granted_status = 'Error setting ACL ' + e.toString(); });
+                });
+            });
+        };
+        $scope.setConfig = function(config) { 
+            console.info('i got a config ', config);
+            s._ajax('GET', 'apps/nikeplus/api/set_config', { config: JSON.stringify(config) }).then(function(x) { 
+                console.log('success ', x);
+                status('configuration chage committed');
+                window.retval = x;
+            }).fail(function(e) {
+                console.error(e);
+                status('error committing change ' + e.toString());
+            });
+        };
+
+        $scope.doStart = function() {
+            s._ajax('GET', 'apps/nikeplus/api/start').then(function(x) { 
+                console.info('App doStart result: ', x); 
+                status('Start command successful'); 
+            }).fail(function(x) { status(' Error ' + x.toString()); });
+        };
+        $scope.doStop = function() {
+            console.log('App doStop');
+            s._ajax('GET', 'apps/nikeplus/api/stop')
+            .then(function(x) { console.info('App Stop result (): ', x); status('Stop command successful'); })
+            .fail(function(x) { status(' Error ' + x.toString()); });
+        };
+        setInterval(function() { 
+            s._ajax('GET','apps/nikeplus/api/is_running').then(function(r) { 
+                sa(function() { 
+                    $scope.runstate = r.running ? 'Running' : 'Stopped';  
+                });
+            }).fail(function(r) {
+                sa(function() { $scope.runstate = 'Unknown'; });
+            });
+        }, 3000);
+
+        // var initialise = function() {
+        //     $("#step-text").hide();
+        //     $("#step-authurl").hide();
+        //     $("#step-pin").hide();
+        //     $("#step-btn-pin").hide();
+        //     $("#step-btn-download").hide();
+        //     $("#step-load").hide();
+        // }
+
+        // var checkConfig = function() {
+        //     if (!box) { u.debug('no box, skipping '); return ;}
+        //     console.log("check if previously authorised, and downloads");
+        //     $("#step-load").show();
+        //     box.getObj('fitbitConfig').then(function (conf) {
+        //         config = conf;
+        //         console.log('found config >> ', config);
+        //         if (!config.get("authToken")) {
+        //             authoriseApp();
+        //         } else {
+        //             console.log(config.get("authToken")[0]);
+        //             setToken(config.get("authToken")[0]);
+        //         }
+        //     }).fail(function(e) {
+        //         u.error('error ', e);
+        //     });
+        // };
+
+        // var authoriseApp = function() {
+        //     $("#step-text").text("The first step is to authorise INDX to access your Fitbit data.");
+        //     $("#step-text").show();
+        //     $.ajax({
+        //         url: "api",
+        //         data: {"gotourl": ""},
+        //         type: "POST",
+        //         dataType: "json",
+        //         success: function(data, status, xhr){
+        //             var gotourl = data.url;
+        //             $("#step-load").hide();
+        //             $("#step-authurl").find("a").attr("href", gotourl);
+        //             $("#step-authurl").show();
+        //             $("#step-pin").show();
+        //             $("#step-btn-pin").show();
+        //         }
+        //     });
+        // };
+
+        // var setToken = function(token) {
+        //     $.ajax({
+        //         url: "api",
+        //         data: {"token": token},
+        //         type: "POST",
+        //         dataType: "json",
+        //         success: function(data, status, xhr){
+        //             showDownloadOption();
+        //         }
+        //     });
+        // };
+
+        // $scope.setPin = function(pin) {
+        //     u.debug("pin is :" + pin);
+        //     $.ajax({
+        //         url: "api",
+        //         data: {"pin": $("#step-pin").find("input").val()},
+        //         type: "POST",
+        //         dataType: "json",
+        //         success: function(data, status, xhr){
+        //             u.debug("token is: " + data.token);
+        //             config.set("authToken", data.token);
+        //             config.save().then(function(h) { u.debug('saved config token' + config.authToken); })
+        //                         .fail(function(e) { u.error('could not save config token'); });
+        //             showDownloadOption();
+        //         }
+        //     });
+        // };
+
+        // var showDownloadOption = function() {
+        //     console.log("show download button");
+        //     $("#step-load").hide();
+        //     $("#step-authurl").hide();
+        //     $("#step-pin").hide();
+        //     $("#step-btn-pin").hide();
+        //     if (config && config.get("authToken")) {
+        //         if (!config.get("upToDate")) {
+        //             $("#step-text").text("INDX was never synced with Fitbit. Click the button below if you would like to start the download now.");
+        //         }
+        //         else {
+        //             lastSync = new Date(parseInt(config.get("upToDate")));
+        //             $("#step-text").text("The last sync was on "+lastSync.toISOString()+". Click the button below if you would like to start the download now.");
+        //         }
+        //         $("#step-text").show();
+        //         $("#step-btn-download").show();
+        //     }
+        // };
+
+        // $scope.startDownload = function() {
+        //     console.log("starting download");
+        //     $("#step-load").show();
+        //     var upToDate, fromDate, observations;
+        //     if (config.get("fromDate")) {
+        //         start = new Date(parseInt(config.get("upToDate")));
+        //         console.log(start.toISOString());
+        //         $.ajax({
+        //             url: "api",
+        //             data: {"download": "true", "start": start.valueOf()},
+        //             type: "POST",
+        //             dataType: "json",
+        //             success: function(data, status, xhr){
+        //                 upToDate = Date.parse(data.upToDate);
+        //                 observations = JSON.parse(data.observations);
+        //                 saveObservations(observations, upToDate);
+        //             }
+        //         });
+        //     } else {
+        //         $.ajax({
+        //             url: "api",
+        //             data: {"download": "true"},
+        //             type: "POST",
+        //             dataType: "json",
+        //             success: function(data, status, xhr){
+        //                 upToDate = Date.parse(data.upToDate);
+        //                 fromDate = Date.parse(data.fromDate);
+        //                 observations = JSON.parse(data.observations);
+        //                 saveObservations(observations, upToDate, fromDate);
+        //             }
+        //         });
+        //     }
+        // };
+
+        // var saveObservations = function(observations, upTo, from) {
+        //     // config.set(each of the observation in the list);
+        //     box.getObj('fitbitDataset').then(function (ds) {
+        //         ds.set({
+        //             // @type: 'http://purl.org/linked-data/cube#Dataset',
+        //             device: 'Fitbit Connector',
+        //         });
+        //         ds.save()
+        //             // .then(function(e) { u.debug('created and saved dataset ' + ds.get('@id')); })
+        //             .fail(function(e) { u.error('could not save dataset ', e); });
+        //         dps = [];
+        //         observations.map(function(obs) {
+        //             st = Date.parse(obs['start']).valueOf();
+        //             box.getObj('fitbitObs_' + st).then(function (o) {
+        //                 o.set({
+        //                     // @type: 'http://purl.org/linked-data/cube#Observation',
+        //                     start: Date.parse(obs['start']).valueOf(), 
+        //                     end: Date.parse(obs['end']).valueOf(),
+        //                     dataset: ds.get('@id'),
+        //                 });
+        //                 if ('stepCount' in obs) {
+        //                     o.set({stepCount: obs['stepCount']});
+        //                 }
+        //                 if ('caloriesBurned' in obs) {
+        //                     o.set({caloriesBurned: obs['caloriesBurned']});
+        //                 }
+        //                 if ('distance' in obs) {
+        //                     o.set({distance: obs['distance']});
+        //                 }
+        //                 if ('floorsClimbed' in obs) {
+        //                     o.set({floorsClimbed: obs['floorsClimbed']});
+        //                 }
+        //                 if ('elevation' in obs) {
+        //                     o.set({elevation: obs['elevation']});
+        //                 }
+        //                 o.save()
+        //                     .then(function(e) { 
+        //                         dps.push(o); // should this be o.get('@id') ? 
+        //                         ds.set({dataPoints: dps});
+        //                         ds.save()
+        //                             .then(function(e) { 
+        //                                 // u.debug('created and saved new dataset ' + ds.get('@id')); 
+        //                             })
+        //                             .fail(function(e) { u.error('could not save new dataset ', e); });
+        //                         // u.debug('created and saved new observation ' + o.get('@id')); 
+        //                     })
+        //                     .fail(function(e) { u.error('could not save new observation ', e); });
+        //             }).fail(function(e) {
+        //                 u.error('could not create new observation ', e);
+        //             });
+        //         });
+        //     }).fail(function(e) {
+        //         u.error('could not create new dataset ', e);
+        //     });
+            
+        //     if (upTo) {
+        //         config.set("upToDate", upTo);
+        //     }
+        //     if (from) {            
+        //         config.set("fromDate", from);
+        //     }
+        //     config.save().then(function(h) { u.debug('saved config dates'); })
+        //                 .fail(function(e) { u.error('could not save config dates'); });
+        //     $("#step-load").hide();
+        // };
+
+        $scope.$watch('selectedUser', function() {
+            if ($scope.selectedUser) {
+                console.log('selected ', $scope.selectedUser);
+                getUsersAndBoxes().then(function() {
+                    getConfigFromService();
+                });
+            }
+        });
+        // window.s = client.store;
+        // initialise();
+    });
+
