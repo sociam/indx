50213228b0dc76c95800dee547acb71ea79b161d
aprilush <aprilush@gmail.com>
2013-12-09 15:26:58 +0000

    more making fitbit async

--- apps/fitbit/bin/fitbit_harvester.py
+++ apps/fitbit/bin/fitbit_harvester.py
@@ -17,6 +17,9 @@ class FitbitHarvester:
         # self.logger = logging.getLogger()
         log_handler = logging.FileHandler("fitbit_harvester.log", "a")
         log_handler.setLevel(logging.DEBUG)
+        
+        formatter = logging.Formatter('%(name)s\t%(levelname)s\t%(asctime)s\t%(message)s')
+        log_handler.setFormatter(formatter)
     
         self.logger = logging.getLogger() 
         self.logger.setLevel(logging.DEBUG)
@@ -38,9 +41,11 @@ class FitbitHarvester:
         consumer_key = "9cc7928d03fa4e1a92eda0d01ede2297"
         consumer_secret = "340ea36a974e47738a335c0cccfe1fcf"
         self.fitbit = Fitbit(consumer_key, consumer_secret)
+        self.fitbit_intraday = None
 
         self.version = 0
         self.ts_count = 0
+        self.overwrite = False;
 
         self.harvester_id = "fitbit_harvester"
         self.steps_ts_id = "fitbit_steps_ts"
@@ -187,51 +192,55 @@ class FitbitHarvester:
         return return_d
          
     def work(self, server_url):
-        start, box, user, password, overwrite = self.check_configuration()
+        start, box, user, password, self.overwrite = self.check_configuration()
         self.logger.debug("Starting download from date: {0}".format(start))
         self.day = datetime.strptime(start, "%Y-%m-%d")+timedelta(days=+1) # adding 1 ecasue we actually start harvesting 1 day early in harvest(..)
 
-        fitbit_intraday = FitbitIntraDay(self.fitbit)
+        self.fitbit_intraday = FitbitIntraDay(self.fitbit)
         self.logger.debug("Created FitbitIntraDay.")
 
         def indx_cb(indx):
             self.logger.debug("Created INDXClient.")
 
-            def timeseries_cb(x):
+            def timeseries_cb(indx):
                 self.logger.debug("Found or created all 5 time series.")
                 
-                def harvester_cb(harvester):
+                def harvester_cb(harvester, indx=indx):
+                    return_d = Deferred()
                     if harvester :
                         if "zeros_from" in harvester :
                             all_zeros_from = datetime.strptime(harvester["zeros_from"][0]["@value"], "%Y-%m-%dT%H:%M:%S")
                             if self.day < all_zeros_from :
-                                if overwrite :
+                                if self.overwrite :
                                     harvester["zeros_from"] = self.day.isoformat()
+                                    self.overwrite = False; # will only overwrite the first time if the flag is set
                         else :
                             harvester["zeros_from"] = self.day.isoformat()
-                    self.safe_update(indx, harvester)
-                    self.harvest(indx, fitbit_intraday)
+                    self.safe_update(indx, harvester).addCallbacks(return_d.callback, return_d.errback)
+                    return return_d
 
-                self.find_create(indx, self.harvester_id, {"http://www.w3.org/2000/01/rdf-schema#label":"INDX Fitbit Harvester extra info"}).addCallbacks(harvester_cb, lambda er: self.logger.error("Error getting harvester: {0}".format(er)))
+                def harvest_cb(x, indx=indx):
+                    def wait():
+                        self.logger.debug("Harvested! Suspending execution for 6 hours at {0}.".format(datetime.now().isoformat()))
+                        sleep(21600)
 
-            if self.ts_count and self.ts_count==5:
-                timeseries_cb()
-            else:
-                self.find_create_timeseries(indx).addCallbacks(timeseries_cb, lambda f: self.logger.error("Failed to find or create one or more of the time series. {0}".format(f)))
+                    self.harvest(indx).addCallbacks(wait, lambda er: self.logger.error("Error during harvesting: {0}".format(er)))
+                
+                self.find_create(indx, self.harvester_id, {"http://www.w3.org/2000/01/rdf-schema#label":"INDX Fitbit Harvester extra info"}).addCallbacks(harvester_cb, lambda er: self.logger.error("Error getting harvester: {0}".format(er))).addCallbacks(harvest_cb, lambda er: self.logger.error("Error during updating harvester: {0}".format(er)))
+            
+            self.find_create_timeseries(indx).addCallbacks(timeseries_cb, lambda f: self.logger.error("Failed to find or create one or more of the time series. {0}".format(f)))
+
+
 
         def loop():
             while 1: 
                 self.get_indx(server_url, box, user, password).addCallbacks(indx_cb, lambda failure: self.logger.error("Error getting indx: {0}".format(failure)))
-                self.logger.debug("Harvested! Suspending execution for 2 hours at {0}.".format(datetime.now().isoformat()))
-                sleep(7200)
-                overwrite = false; # will only overwrite the first time if the flag is set
+                wait()
 
         threads.deferToThread(loop)
 
     def find_create_timeseries(self, indx):
         return_d = Deferred()
-        self.ts_count = 0
-        self.ts_err = []
 
         def s_ts_cb(ts):
             self.ts_count = self.ts_count + 1
@@ -247,7 +256,7 @@ class FitbitHarvester:
                         
                         def f_ts_cb(ts):
                             self.ts_count = self.ts_count + 1
-                            return_d.callback(None)
+                            return_d.callback(indx)
 
                         self.find_create(indx, self.floors_ts_id, {"http://www.w3.org/2000/01/rdf-schema#label":"Fitbit Floors Time Series", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":"http://purl.org/linked-data/cube#Dataset"}).addCallbacks(f_ts_cb, return_d.errback)
     
@@ -257,12 +266,18 @@ class FitbitHarvester:
             
             self.find_create(indx, self.calories_ts_id, {"http://www.w3.org/2000/01/rdf-schema#label":"Fitbit Calories Time Series", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":"http://purl.org/linked-data/cube#Dataset"}).addCallbacks(c_ts_cb, return_d.errback)
 
-        self.find_create(indx, self.steps_ts_id, {"http://www.w3.org/2000/01/rdf-schema#label":"Fitbit Steps Time Series", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":"http://purl.org/linked-data/cube#Dataset"}).addCallbacks(s_ts_cb, return_d.errback)
+        if self.ts_count and self.ts_count==5:
+            return_d.callback(indx)
+        else:
+            self.ts_count = 0
+            self.ts_err = []
+            self.find_create(indx, self.steps_ts_id, {"http://www.w3.org/2000/01/rdf-schema#label":"Fitbit Steps Time Series", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type":"http://purl.org/linked-data/cube#Dataset"}).addCallbacks(s_ts_cb, return_d.errback)
         return return_d
 
-    def harvest(self, indx, fitbit_intraday):
-        
-        def harvester_cb(harvester):
+    def harvest(self, indx):
+        return_d = Deferred()
+
+        def harvester_cb(harvester, indx=indx):
             fetched_days = []
             day = self.today()
             if harvester :
@@ -275,15 +290,10 @@ class FitbitHarvester:
             day = day + timedelta(days=-1) # recheck the last day that was not all zeros, in case only a part of it was synced, this also ensures that at least 1 day of data is fetched
 
             while day < self.today():
-                # skip = False
                 day_points = None
                 if day.date().isoformat() in fetched_days:
                     self.logger.debug("Data for {0} was already fetched, rechecking!".format(day.date().isoformat()))
-                    # self.find_and_delete_points(indx, day)
                     day_points = self.find_day_points(indx, day) # {"dataset_id":{hash based on start time}, "dataset_id":{hash based on start time}, ...}
-                    # else :
-                    #     self.logger.debug("Data for {0} was already fetched, skipping day!".format(day.date().isoformat()))
-                    #     skip = True
                 else:
                     self.logger.debug("Data for {0} was not yet fetched. Getting it now.".format(day.date().isoformat()))
                     fetched_days.append(day.date().isoformat())
@@ -291,7 +301,7 @@ class FitbitHarvester:
                     self.safe_update(indx, harvester)
 
                 # processing steps
-                steps = self.download_steps(day, fitbit_intraday)
+                steps = self.download_steps(day)
 
                 zeros = False
                 zeros = self.check_all_zero(steps)
@@ -313,7 +323,7 @@ class FitbitHarvester:
                 self.save(indx, steps_points)
 
                 # processing calories
-                calories = self.download_calories(day, fitbit_intraday)
+                calories = self.download_calories(day)
                 calories_points = []
                 if day_points and self.calories_ts_id in day_points and day_points[self.calories_ts_id] :
                     calories_points = self.replace_data_points(day, calories, day_points[self.calories_ts_id], self.calories_ts_id, "http://sociam.org/ontology/health/CaloriesBurned")
@@ -323,7 +333,7 @@ class FitbitHarvester:
                 self.save(indx, calories_points)
 
                 # processing distance
-                distance = self.download_distance(day, fitbit_intraday)
+                distance = self.download_distance(day)
                 distance_points = []
                 if day_points and self.distance_ts_id in day_points and day_points[self.distance_ts_id] :
                     distance_points = self.replace_data_points(day, distance, day_points[self.distance_ts_id], self.distance_ts_id, "http://sociam.org/ontology/health/Distance") # subclass of http://purl.org/linked-data/cube#Observation
@@ -333,7 +343,7 @@ class FitbitHarvester:
                 self.save(indx, distance_points)
 
                 # processing floors
-                floors = self.download_floors(day, fitbit_intraday)
+                floors = self.download_floors(day)
                 floors_points = []
                 if day_points and self.floors_ts_id in day_points and day_points[self.floors_ts_id] :
                     floors_points = self.replace_data_points(day, floors, day_points[self.floors_ts_id], self.floors_ts_id, "http://sociam.org/ontology/health/FloorsClimbed") # subclass of http://purl.org/linked-data/cube#Observation
@@ -343,7 +353,7 @@ class FitbitHarvester:
                 self.save(indx, floors_points)
 
                 # processing elevation
-                elevation = self.download_elevation(day, fitbit_intraday)
+                elevation = self.download_elevation(day)
                 elevation_points = []
                 if day_points and self.elevation_ts_id in day_points and day_points[self.elevation_ts_id]:
                     elevation_points = self.replace_data_points(day, elevation, day_points[self.elevation_ts_id], self.elevation_ts_id, "http://sociam.org/ontology/health/Elevation") # subclass of http://purl.org/linked-data/cube#Observation
@@ -356,6 +366,7 @@ class FitbitHarvester:
                 self.logger.debug("Finished harvesting round! Exiting harvest() .. ")
 
         self.find_create(indx, self.harvester_id, {"http://www.w3.org/2000/01/rdf-schema#label":"INDX Fitbit Harvester extra info"}).addCallbacks(harvester_cb, lambda er: self.logger.error("Error getting harvester: {0}".format(er)))
+        return return_d
 
     def parse_list(self, vlist):
         out = [x['@value'] for x in vlist]
@@ -437,43 +448,43 @@ class FitbitHarvester:
         self.logger.debug("Finished replacing values in data points - replaced: {0}, kept: {1}, created: {2}".format(replaced, kept, created))
         return data_points
 
-    def download_steps(self, day, fitbit_intraday):
+    def download_steps(self, day):
         if (day == None):
             self.logger.error("No date given for download.")
             return []
-        steps = fitbit_intraday.get_steps(day)[0]
+        steps = self.fitbit_intraday.get_steps(day)[0]
         self.logger.debug("Retrieved steps data for {0}.".format(day.date()))
         return steps
 
-    def download_calories(self, day, fitbit_intraday):
+    def download_calories(self, day):
         if (day == None):
             self.logger.error("No date given for download.")
             return []
-        calories = fitbit_intraday.get_calories(day)[0]
+        calories = self.fitbit_intraday.get_calories(day)[0]
         self.logger.debug("Retrieved calories data for {0}.".format(day.date()))
         return calories
 
-    def download_distance(self, day, fitbit_intraday):
+    def download_distance(self, day):
         if (day == None):
             self.logger.error("No date given for download.")
             return []
-        distance = fitbit_intraday.get_distance(day)[0]
+        distance = self.fitbit_intraday.get_distance(day)[0]
         self.logger.debug("Retrieved distance data for {0}.".format(day.date()))
         return distance
 
-    def download_floors(self, day, fitbit_intraday):
+    def download_floors(self, day):
         if (day == None):
             self.logger.error("No date given for download.")
             return []
-        floors = fitbit_intraday.get_floors(day)[0]
+        floors = self.fitbit_intraday.get_floors(day)[0]
         self.logger.debug("Retrieved floors data for {0}.".format(day.date()))
         return floors
 
-    def download_elevation(self, day, fitbit_intraday):
+    def download_elevation(self, day):
         if (day == None):
             self.logger.error("No date given for download.")
             return []
-        elevation = fitbit_intraday.get_elevation(day)[0]
+        elevation = self.fitbit_intraday.get_elevation(day)[0]
         self.logger.debug("Retrieved elevation data for {0}.".format(day.date()))
         return elevation
 
@@ -547,6 +558,7 @@ class FitbitHarvester:
 
 
     def safe_update(self, indx, obj) :
+        return_d = Deferred()
         self.logger.debug("Updating object {0} at box version {1}".format(obj, self.version))
 
         def update_cb(resp):
@@ -555,7 +567,12 @@ class FitbitHarvester:
                 if resp["code"] == 201 or resp["code"] == 200:
                     self.version = resp["data"]["@version"]
                     self.logger.debug("Updated objects! new box version: {0}".format(self.version))
-        
+                    return_d.callback(self.version)
+
+        def reupdate_cb(o):
+            self.logger.debug("reupdate: received object: {0}".format(o))
+            return_d.callback(o)
+
         def exception_cb(e):
             self.logger.error("EXCEPTION ? {0}".format(e))
             if isinstance(e.value, urllib2.HTTPError): # handle a version incorrect error, and update the version
@@ -563,36 +580,37 @@ class FitbitHarvester:
                     response = e.value.read()
                     json_response = json.loads(response)
                     self.version = json_response['@version']
-                    self.safe_update(indx, obj) # try updating again now the version is correct
+                    self.safe_update(indx, obj).addCallbacks(reupdate_cb, return_d.errback) # try updating again now the version is correct
                 pass
             else:
                 self.logger.error("Error updating INDX: {0}".format(e))
-                sys.exit(1)   
+                # sys.exit(1)   
+                return_d.errback(e)
 
         indx.update(self.version, obj).addCallbacks(update_cb, exception_cb)
-        
+        return return_d
 
-    def safe_delete(self, indx, objs):
-        try:
-            self.logger.debug("Deleting objects {0} at box version {1}".format(objs, self.version))
-            resp = indx.delete(self.version, objs)
-            self.logger.debug("safe_delete: received response: {0}".format(resp))
-            if resp and "code" in resp and "data" in resp:
-                if resp["code"] == 201: # why is it 201 that is returned??
-                    self.version = resp["data"]["@version"]
-                    self.logger.debug("Deleted objects! new box version: {0}".format(self.version))
-                    return self.version
-        except Exception as e:
-            if isinstance(e, urllib2.HTTPError): # handle a version incorrect error, and update the version
-                if e.code == 409: # 409 Obsolete
-                    response = e.read()
-                    json_response = json.loads(response)
-                    self.version = json_response['@version']
-                    return self.safe_delete(indx, objs) # try updating again now the version is correct
-                pass
-            else:
-                self.logger.error("Error deleting from INDX: {0}".format(e))
-                sys.exit(1)             
+    # def safe_delete(self, indx, objs):
+    #     try:
+    #         self.logger.debug("Deleting objects {0} at box version {1}".format(objs, self.version))
+    #         resp = indx.delete(self.version, objs)
+    #         self.logger.debug("safe_delete: received response: {0}".format(resp))
+    #         if resp and "code" in resp and "data" in resp:
+    #             if resp["code"] == 201: # why is it 201 that is returned??
+    #                 self.version = resp["data"]["@version"]
+    #                 self.logger.debug("Deleted objects! new box version: {0}".format(self.version))
+    #                 return self.version
+    #     except Exception as e:
+    #         if isinstance(e, urllib2.HTTPError): # handle a version incorrect error, and update the version
+    #             if e.code == 409: # 409 Obsolete
+    #                 response = e.read()
+    #                 json_response = json.loads(response)
+    #                 self.version = json_response['@version']
+    #                 return self.safe_delete(indx, objs) # try updating again now the version is correct
+    #             pass
+    #         else:
+    #             self.logger.error("Error deleting from INDX: {0}".format(e))
+    #             sys.exit(1)             
 
     def find_create(self, indx, oid, attrs={}):
         return_d = Deferred()
@@ -601,7 +619,7 @@ class FitbitHarvester:
             if obj is None:
                 self.logger.debug("Object {0} not found! Creating it.".format(oid))
                 attrs.update({"@id":oid})
-                self.safe_update(indx, attrs) # don't care (yet) if this is successful or not .. 
+                self.safe_update(indx, attrs).addCallbacks(return_d.callback, return_d.errback) # don't care (yet) if this is successful or not .. 
                 obj = attrs
             return_d.callback(obj)
         
