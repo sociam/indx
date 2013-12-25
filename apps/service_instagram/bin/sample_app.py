0f48f389ad2f1ebd8836fbcf2c9d70ab8fc3f3aa
Ramine Tinati <raminetinati@gmail.com>
2013-12-25 12:22:35 +0000

    Instagram

---
+++ apps/service_instagram/bin/sample_app.py
@@ -0,0 +1,64 @@
+import bottle
+from bottle import route, post, run, request
+from instagram import client, subscriptions
+
+bottle.debug(True)
+
+CONFIG = {
+    'client_id': 'e118fb9760de432cb97df38babede8d9',
+    'client_secret': 'adec102932e44bb89511a6c33518225c',
+    'redirect_uri': 'http://localhost:8515/oauth_callback'
+}
+
+unauthenticated_api = client.InstagramAPI(**CONFIG)
+
+def process_tag_update(update):
+    print update
+
+reactor = subscriptions.SubscriptionsReactor()
+reactor.register_callback(subscriptions.SubscriptionType.TAG, process_tag_update)
+
+@route('/')
+def home():
+    try:
+        url = unauthenticated_api.get_authorize_url(scope=["likes","comments"])
+        return '<a href="%s">Connect with Instagram</a>' % url
+    except Exception, e:
+        print e
+
+@route('/oauth_callback')
+def on_callback():
+    code = request.GET.get("code")
+    if not code:
+        return 'Missing code'
+    try:
+        access_token, user_info = unauthenticated_api.exchange_code_for_access_token(code)
+        if not access_token:
+            return 'Could not get access token'
+        
+        api = client.InstagramAPI(access_token=access_token)
+        recent_media, next = api.user_recent_media()
+        photos = []
+        for media in recent_media:
+            photos.append('<img src="%s"/>' % media.images['thumbnail'].url)
+        return ''.join(photos)
+    except Exception, e:
+        print e
+
+@route('/realtime_callback')
+@post('/realtime_callback')
+def on_realtime_callback():
+    mode = request.GET.get("hub.mode")
+    challenge = request.GET.get("hub.challenge")
+    verify_token = request.GET.get("hub.verify_token")
+    if challenge: 
+        return challenge
+    else:
+        x_hub_signature = request.header.get('X-Hub-Signature')
+        raw_response = request.body.read()
+        try:
+            reactor.process(CONFIG['client_secret'], raw_response, x_hub_signature)
+        except subscriptions.SubscriptionVerifyError:
+            print "Signature mismatch"
+
+run(host='localhost', port=8515, reloader=True)
