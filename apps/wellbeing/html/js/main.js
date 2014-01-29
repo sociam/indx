6b337e20fe1f1ac4ac5bcd3eab31c6652e747715
electronic Max <electronic@gmail.com>
2014-01-29 03:50:11 +0000

    unstable well

--- apps/wellbeing/html/js/main.js
+++ apps/wellbeing/html/js/main.js
@@ -3,15 +3,16 @@
 
 angular
 	.module('wellbeing', ['ng','indx','infinite-scroll'])
-	// .directive('dayview', function() {
-	// 	return {
-	// 		restrict:'E',
-	// 		scope:{ day:'=' },
-	// 		templateUrl:'/apps/wellbeing/templates/day.html'
-	// 		controller:function($scope) {}
-	// 	};
-	// })
-	.directive('locmap', function() {
+	.directive('dayContainer', function() {
+		return {
+			restrict:'E',
+			scope:{ day:'=' },
+			templateUrl:'/apps/wellbeing/templates/day-container.html',
+			controller:function($scope) {
+
+			}
+		};
+	}).directive('locmap', function() {
 		return {
 			restrict:'E',
 			replace:true,
@@ -38,15 +39,27 @@ angular
 	}).controller('main', function($scope, client, utils) {
 		var u = utils;
 		$scope.days = [];
-		$scope.locations = [];
-		var all_locs = [];		
-		var box;
-		$scope.genLocs = function() {
-			console.log('genlocs >> ', $scope.locations.length, all_locs.length);
-			var i = 0; 
-			while (i < 6 && $scope.locations.length < all_locs.length) {
-				console.log('pushing ', all_locs[$scope.locations.length]);
-				$scope.locations.push(all_locs[$scope.locations.length]);
+		// $scope.locations = [];
+		var box, all_locs = [];	
+
+		var weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+		var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+		$scope.generatePast = function(start_date, num_days) {
+			console.log('generatePast >> ', start_date);
+			var i = 1; 
+			if (start_date === undefined) { 
+				start_date = new Date(new Date().valueOf() + i*24*3600*1000); 
+			} 
+			while (i <= num_days) {
+				var date = new Date(start_date.valueOf() - i*24*3600*1000);
+				$scope.days.push({ 
+					date:date,
+					dow: weekday[date.getDay()].toLowerCase(),
+					dom:date.getDate(),
+					month:months[date.getMonth()].toLowerCase().slice(0,3),
+					things:[] 
+				});
 				i++;
 			}
 		};
@@ -56,7 +69,6 @@ angular
 			box = client.store.getBox($scope.box).then(function(_box) { 
 				window.box = _box;
 				box = _box;
-				console.log('querying ----------- > ');
 				box.query({type:'location'}).then(function(things) {
 					u.safeApply($scope, function() { 
 						all_locs = things.concat(); 
@@ -64,6 +76,7 @@ angular
 					});
 				}).fail(function(bail) { console.log('fail querying ', bail); });				
 				// $scope.genDay();
+				u.safeApply($scope, function() { $scope.generatePast(undefined,4); });
 			}).fail(function(bail) { console.error(bail); });
 		});
 
