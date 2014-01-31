83db82f72749773768f1228fed7516cdac689f7f
Peter West <peter.g.west@gmail.com>
2014-01-31 20:19:53 +0000

    dropdown stuff

--- apps/bettertodos/html/todos.js
+++ apps/bettertodos/html/todos.js
@@ -164,8 +164,9 @@ angular
 		};
 
 		$scope.bodyClick = function () {
+			console.log(this)
 			$scope.lists.forEach(function (list) {
-				//list.showDropdown = false;
+				list.showDropdown = false;
 			});
 		};
 
@@ -263,4 +264,18 @@ angular
 				});
 			}
 		};
-	});;
+	}).directive('clickElsewhere', function($document){
+		return {
+		restrict: 'A',
+		link: function(scope, elem, attr, ctrl) {
+			elem.bind('click', function(e) {
+				// this part keeps it from firing the click on the document.
+				e.stopPropagation();
+			});
+			$document.bind('click', function() {
+				// magic here.
+				scope.$apply(attr.clickElsewhere);
+			})
+			}
+		}
+	});
