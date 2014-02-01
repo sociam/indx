ea3545e2392d5938e2d7fb0c27aea8b6e0e8bcc6
electronic Max <max@hip.cat>
2014-02-01 19:00:59 +0000

    page2page

--- apps/aditl/html/js/main.js
+++ apps/aditl/html/js/main.js
@@ -2,61 +2,8 @@
 /* global require, exports, console, process, module, L, angular, _, jQuery */
 
 angular
-	.module('wellbeing', ['ng','indx','infinite-scroll','ngAnimate'])
-	.directive('pages2page', function() { 
-		return {
-			restrict:'E',
-			scope:{ page:'=' },
-			replace:true,
-			templateUrl:'/apps/aditl/templates/pages2page.html',
-			controller:function($scope, utils) {
-				// literally nothing necessary here.
-			}
-		};
-	}).controller('pages2', function($scope, $injector, client, utils, entities) { 
-		$scope.$watch('user + box', function() { 
-			var u = utils;
-			var sa = function(fn) { return u.safeApply($scope, fn); };
-			$scope.pages = [];
-			var appenddebug = function(model) { 
-				var url = model.peek('thumbnail');
-				var row = ['<tr>','<td class="url">',model.id.slice(0,100),'</td>','<td>',url && url.length,'</td><td class="data"><a href="',url && url,'">',url && url.slice(0,300),'</a></td></tr>'].join('');
-				console.log('appending row ', row);
-				jQuery('#debugtable').append(row);
-			};
-			var prepend = function(model){
-				var parent = jQuery('.pages');
-				var datauri = model.peek('thumbnail');
-				sa(function() { $scope.pages.push(model); });
-				appenddebug(model);
-			};
-			client.store.getBox($scope.box).then(function(_box) { 
-				_box.on('obj-add', function(id) { 
-					_box.getObj(id).then(function(model) { 
-						if (model && model.peek('type') == 'web-page') { 
-							console.log("new Page prepending >> ", model.id);
-							prepend(model);
-						} else {
-							console.log('uncaching obj ', model.id);
-							_box.uncacheObj(model);
-						}
-					});
-				});
-				entities.documents.getWebPage(_box).then(function(pages) {
-					console.log('got pages >> ', pages.length, pages);
-					window.pages = pages;
-					pages.map(function(page) { 
-						prepend(page); 
-					});
-				});
-			}).fail(function(bail) { console.error(bail); });
-		});
-		window.scope = $scope;
-		// $scope.addImg = function(thumbobj) {
-		// 	var data = $scope.decodeThumb(thumbobj);
-		// 	jQuery(".pages").append("<img class='page' data-add='manual' style='border: 1px solid red' src='"+data+"''>");
-		// };
-	}).controller('pages', function($scope, client, utils, entities) { 
+	.module('aditl', ['ng','indx','infinite-scroll','ngAnimate'])
+	.controller('pages', function($scope, client, utils, entities) { 
 		$scope.$watch('user + box', function() { 
 			var u = utils;
 			var sa = function(fn) { return u.safeApply($scope, fn); };
