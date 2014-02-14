061ff8c65615dc8467fd58e9efa04df6f049d622
electronic Max <max@hip.cat>
2014-02-14 14:34:40 +0000

    new css and new life

--- html/js/launcher-box.js
+++ html/js/launcher-box.js
@@ -8,17 +8,31 @@ angular.module('launcher')
 			link:function($scope, $element) {
 				$scope.el = $element;
 			},
-			scope: { box:"=box" },
+			scope: { box:"=b" },
 			controller: function($scope, client, backbone, utils) {
 				var u = utils, sa = function(f) { utils.safeApply($scope,f); };
+				var guid = u.guid(), old_box;
+
 				$scope.obj_ids = $scope.box ? $scope.box.getObjIDs() : [];
 				$scope.obj_count = $scope.obj_ids.length;
-				$scope.box.on('obj-add', function(result) { 
-					// console.log('obj add ', result, typeof(result));
-					sa(function() { $scope.obj_count++; });
-				});
-				$scope.box.on('obj-remove', function(result) { 
-					sa(function() { $scope.obj_count--; });
+
+				$scope.$watch('box', function() { 
+					console.log('boxxxxx ', $scope.box);
+					if (!$scope.box) { return; }
+					if (old_box) { old_box.off(undefined, undefined, guid); }
+
+					$scope.obj_ids = $scope.box ? $scope.box.getObjIDs() : [];
+					$scope.obj_count = $scope.obj_ids.length;
+
+					$scope.box.on('obj-add', function(result) { 
+						// console.log('obj add ', result, typeof(result));
+						sa(function() { $scope.obj_count++; });
+					}, guid);
+					$scope.box.on('obj-remove', function(result) { 
+						sa(function() { $scope.obj_count--; });
+					}, guid);
+					old_box = $scope.box;
+
 				});
 				$scope.deleteBox = function() {
 					console.log("Attempting to delete box ", $scope.box);
