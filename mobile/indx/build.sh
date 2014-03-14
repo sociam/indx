e35ddb63d506474f71e3b3ca4f4c7565d99d4a36
electronic Max <max@hip.cat>
2014-03-14 11:07:03 +0000

    added build script

---
+++ mobile/indx/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# these directories aren't checked out by default
+mkdir merges plugins platforms
+cordova create .
+cordova platforms add ios
+cordova plugins add org.apache.cordova.camera
+cordova plugins add org.apache.cordova.console
+(cd ..; git checkout https://github.com/wildabeast/BarcodeScanner.git)
+cordova plugins add ../BarcodeScanner
+cordova prepare
+echo "done!"
+
+
+
