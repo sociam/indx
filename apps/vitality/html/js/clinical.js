50fcab25608aef97788f7b9b38df3f9684398b3c
electronic Max <electronic@gmail.com>
2014-04-15 22:00:25 +0100

    scribble

--- apps/vitality/html/js/clinical.js
+++ apps/vitality/html/js/clinical.js
@@ -34,10 +34,51 @@
 			},
 			link:function($scope, $element) {
 				var el = $element[0], div = $element[0], last_length = 0;
-				$scope.svg = d3.select(el).append('svg').attr('width', 500).attr('height', 800);
-				el.on('touchstart', function(evt) { 
+				var data = [];
+				var cur_line = undefined;
+				var svg = d3.select(el).append('svg').attr('width', 500).attr('height', 800);
+				var linefn = d3.svg.line()
+					.x(function(d) { return d.x; })
+					.y(function(d) { return d.y; })
+					.interpolate('linear');
+
+				var render = function() { 
+
+					svg.selectAll('path').remove();
+					(cur_line ? data.concat([cur_line]) : data).map(function(D) {
+						console.log('rendering >> ', D, D.length);
+						svg.append('path')
+							.attr('d', linefn(D))
+							.attr('stroke','blue')
+							.attr('stroke-width','2')
+							.attr('fill', 'none');
+					});
+				};
+
+				var evt2pos = function(evt) { 
+					var offset = $(svg[0]).offset();
+					return { x: evt.originalEvent.touches[0].clientX - offset.left, y: evt.originalEvent.touches[0].clientY - offset.top };
+				};
+
+				$(el).on('touchstart', function(evt) { 
 					console.log('touchstart >> ', evt);
+					cur_line = [ evt2pos(evt) ];
+					render();
 				});
+				$(el).on('touchend', function(evt) { 
+					console.log('<< touchend >> ', evt);
+					data.push(cur_line);
+					cur_line = undefined;
+					render();
+				});
+				$(el).on('touchmove', function(evt, e) {
+					// console.log('<< touchdrag ', evt, e);
+					evt.preventDefault();					
+					window.evt = evt; 
+					cur_line.push(evt2pos(evt));
+					render();					
+				});
+				window.data = data;
 			}
 		};
 	});
