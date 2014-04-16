7150838089ad31de90b2cbf6e08b2172972532d4
electronic Max <electronic@gmail.com>
2014-04-16 16:42:45 +0100

    scribbles

--- apps/vitality/html/js/clinical.js
+++ apps/vitality/html/js/clinical.js
@@ -26,61 +26,91 @@
 			restrict:'E',
 			replace:true,
 			scope:{model:"="},
-			template:'<div class="scribbler"></div>',
+			template:'<div class="scribbler"><div class="buttons"><div ng-show="selected_path" class="btn glyphicon glyphicon-remove" ng-click="deleteSelected()"></div><div ng-show="data.length > 0" class="btn glyphicon glyphicon-backward" ng-click="deleteLast()"></div></div></div>',
 			controller:function($scope, utils)  {
+
 				var u = utils, 
-				sa = function(fn) { return u.safeApply($scope, fn); },
-				$s = $scope;
+					sa = function(fn) { return u.safeApply($scope, fn); },
+					$s = $scope; 
+
+
+				$scope.$watch('element', function(element) { 
+					console.log('element >> ', el);
+					var el = element[0], data = [];
+					$scope.data = data;
+					var cur_line = undefined;
+					var svg = d3.select(el).append('svg'); // .attr('width', 500).attr('height', 800);
+					var offset;
+					var linefn = d3.svg.line()
+						.x(function(d) { return d.x; })
+						.y(function(d) { return d.y; })
+						.interpolate('linear');
+
+					var render = function() { 
+						svg.selectAll('path.drawing').remove();
+						if (cur_line) {
+							svg.append('path')
+								.attr('class', 'scribble drawing')
+								.attr('d', linefn(cur_line))
+								.data(cur_line);
+						}
+					};
+					var evt2pos = function(evt) { 
+						if (!offset) { offset = $(svg[0]).offset(); }
+						return { x: evt.originalEvent.touches[0].clientX - offset.left, y: evt.originalEvent.touches[0].clientY - offset.top };
+					};
+					var clear_select = function() { 
+						d3.selectAll('.scribble.selected').attr('class', 'scribble');
+						sa(function() { delete $scope.selected_path; });
+					};
+				
+					var click_handler = function(evt){
+						var this_ = this;
+						clear_select();
+						// select!
+						d3.select(this).attr('class', 'scribble selected');
+						sa(function() { $scope.selected_path = this_; });
+					};
+
+					$(svg[0]).on('touchstart', function(evt) { 
+						clear_select();
+						cur_line = [ evt2pos(evt) ];
+					});
+					$(svg[0]).on('touchend', function(evt) { 
+						console.log('<< touchend >> ', evt);
+						sa(function() { data.push(cur_line); });
+						svg.selectAll('path.drawing')
+							.attr('class', 'scribble')
+							.on('click', click_handler);
+						cur_line = undefined;
+					});
+					$(svg[0]).on('touchmove', function(evt, e) {
+						evt.preventDefault();					
+						cur_line.push(evt2pos(evt));
+						render();
+					});
+					$scope.deleteSelected = function() { 
+						var selected = $scope.selected_path;
+						var selected_points = d3.select(selected).attr('data-points');
+						console.log('selected_points >> ', selected_points);
+						window.dd = selected_points;
+						d3.select(selected).remove();
+						clear_select();
+						return true;
+					};
+					$scope.deleteLast = function() { 
+						var paths = svg.selectAll('path.scribble')[0];
+						if (paths.length > 0) {
+							d3.select(paths[paths.length -1]).remove();
+						}
+						clear_select();
+						return true;
+					};
+
+				}); // watch element
 			},
 			link:function($scope, $element) {
-				var el = $element[0], div = $element[0], last_length = 0;
-				var data = [];
-				var cur_line = undefined;
-				var svg = d3.select(el).append('svg'); // .attr('width', 500).attr('height', 800);
-				var offset;
-				var linefn = d3.svg.line()
-					.x(function(d) { return d.x; })
-					.y(function(d) { return d.y; })
-					.interpolate('linear');
-
-				var render = function() { 
-					svg.selectAll('path.drawing').remove();
-					if (cur_line) {
-						svg.append('path')
-							.attr('class', 'scribble drawing')
-							.attr('d', linefn(cur_line));
-					}
-				};
-
-				var evt2pos = function(evt) { 
-					if (!offset) { offset = $(svg[0]).offset(); }
-					return { x: evt.originalEvent.touches[0].clientX - offset.left, y: evt.originalEvent.touches[0].clientY - offset.top };
-				};
-
-				$(el).find('path.scribble').on('touchstart', function(evt) { 
-					console.error('YOOOOOOOOO scribble click!! ', this, evt.currentTarget);
-					$('.scribble.selected').removeClass('.selected');
-					$(this).addClass('selected');
-					evt.preventDefault();					
-				});
-				$(el).on('touchstart', function(evt) { 
-					console.log('touchstart >> ', evt);
-					cur_line = [ evt2pos(evt) ];
-				});
-				$(el).on('touchend', function(evt) { 
-					console.log('<< touchend >> ', evt);
-					data.push(cur_line);
-					svg.selectAll('path.drawing').attr('class', 'scribble');
-					cur_line = undefined;
-				});
-				$(el).on('touchmove', function(evt, e) {
-					// console.log('<< touchdrag ', evt, e);
-					evt.preventDefault();					
-					window.evt = evt; 
-					cur_line.push(evt2pos(evt));
-					render();
-				});
-				window.data = data;
+				$scope.element = $element;
 			}
 		};
 	});
