7dbc13f4ebbdfedc79f12d3fdcd92ba614d43f4b
electronic Max <electronic@gmail.com>
2014-04-21 13:32:11 +0100

    indexeddb progress

--- html/js/indx-indexeddb.js
+++ html/js/indx-indexeddb.js
@@ -1,5 +1,6 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false, -W110 */
 /* global $,_,document,window,console,escape,Backbone,exports,WebSocket,process,_NODE_AJAX,angular,jQuery */
+
 ///  @title indx-offline.js
 ///  @author Max Van Kleek
 ///  @since 2014
@@ -10,8 +11,7 @@
 ///  read and write objects from one or more INDX data store(s).
 
 ///  #### Copying
-///  Copyright (C) 2011-2013 University of Southampton
-///  Copyright (C) 2011-2013 Max Van Kleek
+///  Copyright (C) 2011-2014 Max Van Kleek
 ///
 ///  This program is free software: you can redistribute it and/or modify
 ///  it under the terms of the GNU Affero General Public License, version 3,
@@ -35,30 +35,29 @@ angular
 		
 		function deleteDB(dbid) {
 		    try {
-
 		        var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-
 		        var dbreq = indexedDB.deleteDatabase(dbid);
 		        dbreq.onsuccess = function (event) {
 		            var db = event.result;
-		            console.log("indexedDB: " + dbid + " deleted");
-		        }
+		        };
 		        dbreq.onerror = function (event) {
 		            console.error("indexedDB.delete Error: " + event.message);
-		        }
+		        };
 		    }
 		    catch (e) {
 		        console.error("Error: " + e.message);
 		        //prefer change id of database to start ont new instance
-		        dbid = dbid + "." + fallBackDBGuid;
-		        console.log("fallback to new database name :" + dbid)
+		        console.log("fallback to new database name :" + dbid);
 		    }
 		}
 
 		var Store = Backbone.Model.extend({
-			getBox:function(bid) { 
+			initialize:function() { 
+				this.collections_by_boxid = {};
+				this.deferreds_by_boxid = {};
+			},
+			_makeBox:function(bid, make_indexes) { 
 				u.assert(bid !== undefined, "box id has to be defined");
-
 				var db = {
 			 		id:bid,
 			 		description:'indx local store',
@@ -66,42 +65,106 @@ angular
 				 		{
 				 			version:1,
 				 			migrate:function(transaction, next){
-				 				console.log('migrate >> ', transaction);
 				 				var store = transaction.db.createObjectStore(bid);
 				 				next();
 				 			}
 				 		}
 				 	]			 			
-				 };
+				};
+				if (make_indexes !== undefined) {
+					db.migrations.push({
+						version:2,
+						migrate:function(transaction, next) { 
+							try {
+								var store = transaction.objectStore(bid);
+								make_indexes.map(function(field) { 
+									store.createIndex(field + "index", field, { unique: false });
+						        });
+						        next();
+						    } catch(e) { console.error(e); }
+						}
+					});
+				}
+			 	var DBModel = Backbone.Model.extend({ database: db, storeName:bid	}),
+				 	Collection = Backbone.Collection.extend({ 
+				 		database:db,
+				 		storeName:bid,
+				 		model: DBModel,
+				 		getObj:function(id) {
+				 			if (this.get(id) !== undefined) { 
+				 				return u.dresolve(this.get(id));
+				 			}
+				 			console.log('doesnt exist, making new ');
+				 			return u.dresolve(new this.model({id:id}));
+				 		}
+				 	});
+				this.collections_by_boxid[bid] = Collection;
 
-			 	var DBModel = Backbone.Model.extend({  
-		 			database: db,
-			 		storeName:bid
-		 		}), 
-			 	Collection = Backbone.Collection.extend({ 
-			 		database:db,
-			 		storeName:bid,
-			 		model: DBModel
-			 	}), 
-			 	d = u.deferred(), 
-			 	c = new Collection();
-
-			 	c.fetch().then(function() { d.resolve(c); }).fail(d.reject);
-
+			 	var collection = new Collection(), d = u.deferred();
+			 	collection.fetch().then(function() {
+			 		d.resolve(collection); 
+			 	}).fail(d.reject);
 			 	return d.promise();
+			},
+			getBox:function(bid, make_indexes) { 
+				if (this.deferreds_by_boxid[bid]) {
+					return this.deferreds_by_boxid[bid];
+				} 
+				this.deferreds_by_boxid[bid] = this._makeBox(bid, make_indexes);
+				return this.deferreds_by_boxid[bid];
 			}
 		});
 
+
+		var s, 
+		getStore = function() { 
+				if (!s) { s = new Store(); }
+				return s; 
+		},
+		test3 = function(id) {
+			var s = getStore();
+			var boxid = id || u.guid(6);
+			s.getBox(boxid,['name']).then(function(box) {
+				var a = window.as = [];
+				var ds = u.range(100).map(function(i) { 
+					var guid = u.guid(6), d_ = u.deferred();
+					box.getObj(guid).then(function(x) {
+						a.push(x);
+						console.log('got obj >> ', x.id);
+						x.set({name:['hello','hi', 'goodbye'][i%3], date:i});
+						x.save().then(d_.resolve);
+					});
+					return d_.promise();
+				});
+				u.when(ds).then(function() { 
+					var query = new s.collections_by_boxid[boxid]();
+					query.fetch({
+						conditions: {name:'hello'},
+						success: function () {
+							console.log('wins ', query.models.length, query.models);
+							console.log(query.models.map(function(x) { return x.id; }));
+                            // equal(query.models.length, 2, "Should have 2 elements");
+                            // deepEqual(theater.pluck("title"), ["Bonjour", "Ciao"], "Should have [\"Bonjour\", \"Ciao\"]");
+                        },
+						error: function (object, error) {
+                            console.error('couldnt query -- ', object, error);
+                        }                            
+					}).then(function() { 
+						console.log('hello and hi hits ', query.length);
+					}).fail(function(r) { 
+						console.error(r); 
+					});
+				}).fail(function(x) { console.error('sad '); });
+			}).fail(function(err) {	console.error('couldnt get box '); 	});
+		};
+		setTimeout(function() { test3(); }, 100);
+
 		return {
-			getStore : function() { 
-				return new Store(); 
-			},
-			test : function() { 
+			getStore : getStore,
+			test : function(boxid) { 
 				var s = this.getStore();
-				s.getBox('a').then(function(box) {
-					console.log('box');
+				s.getBox(boxid || 'a').then(function(box) {
 					window.box = box;
-					console.log('box has ', box.size());
 					var ds = [];
 					u.range(10).map(function(x) {
 						var uid = u.guid(10);
@@ -111,11 +174,26 @@ angular
 					});
 					u.when(ds)
 						.then(function(x) { console.log('all done!'); })
-						.fail(function(x) { console.error(e); });
+						.fail(function(e) { console.error(e); });
 				}).fail(function(err) { 
 					console.error('couldnt get box '); 
 				});
 			},
+			test2 : function(boxid) { 
+				var s = this.getStore();
+				s.getBox(boxid || 'a').then(function(box) {
+					var guid = u.guid();
+					box.getObj(guid).then(function(x) {
+						console.log('created x >> ');
+						u.assert(x.id == guid, 'ids didnt match');
+						x.set({baby:true});
+						x.save();
+					});
+				}).fail(function(err) {
+					console.error('couldnt get box '); 
+				});
+			},
+			test3 : test3,
 			deleteDB:deleteDB
 		};
 
