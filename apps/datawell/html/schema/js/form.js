973d64efc87a836026403ef1ae3ab5986616d05b
electronic Max <max@hip.cat>
2014-04-26 02:13:23 +0100

    working towards form

--- apps/datawell/html/schema/js/form.js
+++ apps/datawell/html/schema/js/form.js
@@ -1,39 +1,33 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false, -W110 */
 /*global $,_,document,window,console,escape,Backbone,exports,WebSocket,process,_NODE_AJAX,angular,jQuery */
 
-angular.module('datawell', ['indx'])
-	.directive('DataWellInputForm', function() { 
+angular.module('datawell')
+	.directive('fmlform', function() { 
 		return {
-			replace:true,
 			restrict:'E',
-			scope:{src:'='},
+			scope:{ formsrc:'@' },
 			templateUrl:'partials/base.html',
 			controller:function($scope, fmlParser, utils) {
 				var u = utils, sa = function(fn) { return u.safeApply($scope, fn); };
-				if ($scope.src !== undefined) {
-					$.get($scope.src)
-						.then(function(schematext) { 
-							var tree = fmlParser.parse(schematext); 
-
-						}).fail(function(x) { console.error(x); });
+				if ($scope.formsrc !== undefined) {
+					console.info('loading from ', $scope.formsrc);
+					$.get($scope.formsrc).then(function(schematext) { 
+						var tree = fmlParser.parse(schematext); 
+						if (tree !== undefined) {
+							sa(function() { $scope.root = tree;	});
+						}
+					}).fail(function(x) { console.error(x); });
+				} else {
+					console.error('no src provided');
 				}
 			}
 		};
-	}).directive('DataWellInputComponent', function() { 
-		return {
-			replace:true,
-			restrict:'E',
-			scope:{src:'='},
-			templateUrl:'partials/component.html',
-			controller:function($scope, utils, prov) {
-				var u = utils, sa = function(fn) { return u.safeApply($scope, fn); };
-				if ($scope.src !== undefined) {
-					$.get($scope.src)
-						.then(function(schematext) { 
-							var tree = fmlParser.parse(schematext); 
-
-						}).fail(function(x) { console.error(x); });
-				}
-			}
-		};	
+	}).directive('component', function() { 
+		return { restrict:'E', scope:{root:'='}, templateUrl:'partials/component.html' };	
+	}).directive('group', function() { 
+		return { replace:true, restrict:'E', scope:{root:'='}, templateUrl:'partials/group.html' };	
+	}).directive('category', function() { 
+		return { replace:true, restrict:'E', scope:{root:'='}, templateUrl:'partials/category.html' };	
+	}).directive('inputItem', function() { 
+		return { replace:true, restrict:'E', scope:{root:'='}, templateUrl:'partials/inputitem.html' };
 	});
