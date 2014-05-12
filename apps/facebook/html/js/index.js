5596f8ceeae043fdb064317a7cf9a76443c6eb9f
electronic Max <electronic@gmail.com>
2014-05-12 04:06:57 +0100

    small changes

--- apps/facebook/html/js/index.js
+++ apps/facebook/html/js/index.js
@@ -58,7 +58,7 @@ angular.module('facebook', ['indx'])
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) {return;}
          js = d.createElement(s); js.id = id;
-         js.src = '//connect.facebook.net/en_US/sdk/debug.js';
+         js.src = '//connect.facebook.net/en_US/sdk.js';
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
 
