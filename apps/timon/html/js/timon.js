420e066e378bd9fa93f380677a64e44a05e29cd5
electronic Max <electronic@gmail.com>
2014-08-08 15:22:07 -0400

    timon cleanup

--- apps/timon/html/js/timon.js
+++ apps/timon/html/js/timon.js
@@ -1,37 +1,7 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false, -W110 */
 /* global require, exports, console, process, module, describe, it, expect, jasmine, angular, _, $ */
 angular.module('timon',['indx', 'ngAnimate'])
-	.directive('entercall', function() { 
-		return {
-			link:function($element, $attributes) { 
-				console.log('link .. ', $element, $attributes);
-			},
-			controller:function($scope, $element, $attrs) { 
-				var el = $element[0],
-					fn = $attrs.entercall;
-				console.log('element >> ', $scope, el, fn);
-				$(el).on('keydown', function(evt) { 
-					if (evt.keyCode == 13) { 
-						$scope.$eval(fn); 
-						evt.preventDefault();
-					}
-				});
-			}
-		};
-	}).filter('orderObjectBy', function() {
-		return function(items, field, reverse) {
-		    var filtered = [];
-		    angular.forEach(items, function(item) { filtered.push(item);  });
-		    filtered.sort(function (a, b) {
-		      var av = a.peek(field), bv = b.peek(field);
-		      if (_.isDate(av)) { av = av.valueOf(); }
-		      if (_.isDate(bv)) { bv = bv.valueOf(); }
-		      return (av > bv ? 1 : -1);
-		    });
-		    if(reverse) filtered.reverse();
-		    return filtered;
-		};
-	}).controller('main', function($scope, client, utils, transformers) { 
+	.controller('main', function($scope, client, utils, transformers) { 
 		
 		var store = client.store, 
 			u = utils, box, diffQs = [],
