023d1dbd0b21777a32b0fe4e24484bc6b829db1b
electronic Max <electronic@gmail.com>
2014-08-20 18:24:17 +0100

    added ot main indx

--- apps/surveyor/html/js/index.js
+++ apps/surveyor/html/js/index.js
@@ -24,13 +24,11 @@ var app = angular.module('sharing', ['indx', 'ui.router'])
       			},
 		      	controller:function($scope, $state, $stateParams) { 
 		      		var qid = $stateParams.qid, 
-						uid = $stateParams.uid;
-
+						uid = $stateParams.uid,
+						saveResponse = function(qid, uid, resp) {
+							console.log('save response ', qid, uid, resp);
+						};
 					$scope.response = {};
-					var saveResponse = function(qid, uid, resp) {
-						console.log('save response ', qid, uid, resp);
-					};
-
 		      		$scope.next = function() {
 		      			var next_qid = (parseInt(qid)+1)+"";
 		      			// save response
@@ -52,4 +50,33 @@ var app = angular.module('sharing', ['indx', 'ui.router'])
 	}).controller('main', function($scope, utils) {
 		var u = utils, sa = function(f) { u.safeApply($scope, f); };
 		console.log('hello main starting');
-	});
+	}).directive('radio', function() { 
+		return {
+			restrict:'E',
+			scope:{	options:'@', model:'=' },
+			templateUrl:'tmpl/radio.html',
+			controller:function($scope) { 
+				console.log('scope options ', $scope.options);
+				$scope.model = 'twink';
+				$scope.opts = $scope.options.split(',');
+				$scope.setModel= function(v)  {
+					console.log('setmodel ', v);
+					$scope.model = v;
+				};
+				$scope.$watch('model', function()  {
+					console.log('model change ', $scope.model);
+				});
+			},
+			link:function($scope, $element) { 
+				var m = $scope.model;
+				if (m) { 
+					console.log('already have a value!', m, $element[0]);
+					var opts = $($element[0]).find('.radio-option[value="'+m+'"]');
+					console.log('opts ', opts.length);
+					opts.addClass('active');
+				}
+			}
+		};
+	}).run(function() {
+    FastClick.attach(document.body);
+  });
